f8979982fed57d0ae03b62b98863d1ae688c3617
DESKTOP-T3UG4TB\tangyi <tangyi@nuls.io>
2018-10-16 15:02:20 +0800

    rpc-tools

--- tools/nuls-tools/rpc-tools/src/main/java/io/nuls/rpc/pojo/GetIp.java
+++ tools/nuls-tools/rpc-tools/src/main/java/io/nuls/rpc/pojo/GetIp.java
@@ -37,19 +37,6 @@ import java.util.Enumeration;
  */
 public class GetIp {
 
-    public static void main(String[] args) {
-        try {
-            //使用InetAddress.getLocalHost().getHostAddress()打印
-            System.out.println("InetAddress.getLocalHost().getHostAddress():" + InetAddress.getLocalHost().getHostAddress());
-            System.out.println("打印所有IP列表信息：");
-            GetIp ip = new GetIp();
-            System.out.println("本机地址：" + ip.getIpAdd());
-
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-
     /**
      * 根据网卡获得IP地址
      * @return ip
