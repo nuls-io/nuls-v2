918d63df906a0f54cae6e8853e076a3070ee8f07
tangyi <tangyi@nuls.io>
2018-11-15 20:06:35 +0800

    websocket

--- tools/nuls-tools/rpc-tools/src/main/java/io/nuls/rpc/client/WsClient.java
+++ tools/nuls-tools/rpc-tools/src/main/java/io/nuls/rpc/client/WsClient.java
@@ -61,6 +61,7 @@ public class WsClient extends WebSocketClient {
             /*
              * add to response queue, Waiting for thread pool processing
              */
+            Log.info("Client received: " + paramString);
             Log.info("Client received: " + JSONUtils.json2map(paramString));
             RuntimeInfo.RESPONSE_QUEUE.add(JSONUtils.json2map(paramString));
         } catch (IOException e) {
