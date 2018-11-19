a79630c7aa62f288e663f6dad1b389a96184e57f
wangko27 <wangkun@nuls.io>
2018-11-19 11:18:26 +0800

    lg_test cmd call

--- ledger/nuls-ledger/src/main/java/io/nuls/ledger/rpc/cmd/LedgerCmd.java
+++ ledger/nuls-ledger/src/main/java/io/nuls/ledger/rpc/cmd/LedgerCmd.java
@@ -1,13 +1,37 @@
 package io.nuls.ledger.rpc.cmd;
 
+import io.nuls.ledger.service.LedgerService;
 import io.nuls.rpc.cmd.BaseCmd;
+import io.nuls.rpc.model.CmdAnnotation;
+import io.nuls.rpc.model.CmdResponse;
+import io.nuls.tools.core.annotation.Autowired;
 import io.nuls.tools.core.annotation.Component;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  * Created by wangkun23 on 2018/11/19.
  */
 @Component
 public class LedgerCmd extends BaseCmd {
+    final Logger logger = LoggerFactory.getLogger(getClass());
 
+    @Autowired
+    private LedgerService ledgerService;
 
+    @CmdAnnotation(cmd = "lg_test", version = 1.0, preCompatible = true)
+    public CmdResponse testCmd(List params) {
+        for (Object param : params) {
+            logger.info("param {}", param);
+        }
+
+        Map<String, String> data = new HashMap<>();
+        data.put("foo", "foo");
+        data.put("bar", "bar");
+        return success(0, "", data);
+    }
 }
---
+++ ledger/nuls-ledger/src/main/java/io/nuls/ledger/service/LedgerService.java
@@ -0,0 +1,12 @@
+package io.nuls.ledger.service;
+
+import io.nuls.tools.core.annotation.Service;
+
+/**
+ * Created by wangkun23 on 2018/11/19.
+ */
+@Service
+public class LedgerService {
+
+
+}
