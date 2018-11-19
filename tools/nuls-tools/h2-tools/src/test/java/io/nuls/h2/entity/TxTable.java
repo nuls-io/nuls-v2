ee73128a9ade01ca19a2626a0794f784b5078409
Charlie <29900478@qq.com>
2018-11-19 15:27:25 +0800

    update H2 test

---
+++ tools/nuls-tools/h2-tools/src/test/java/io/nuls/h2/entity/TxTable.java
@@ -0,0 +1,36 @@
+package io.nuls.h2.entity;
+
+/**
+ * @author: Charlie
+ * @date: 2018/11/18
+ */
+public class TxTable {
+    private String tableName;
+
+    private String indexName;
+
+    public TxTable(){
+
+    }
+
+    public TxTable(String tableName, String indexName) {
+        this.tableName = tableName;
+        this.indexName = indexName;
+    }
+
+    public String getTableName() {
+        return tableName;
+    }
+
+    public void setTableName(String tableName) {
+        this.tableName = tableName;
+    }
+
+    public String getIndexName() {
+        return indexName;
+    }
+
+    public void setIndexName(String indexName) {
+        this.indexName = indexName;
+    }
+}
