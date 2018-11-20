391b79af63da8432e05d112a7931da19ab995d56
wangko27 <wangkun@nuls.io>
2018-11-20 18:12:32 +0800

    save pendingState transaction

---
+++ ledger/nuls-ledger/src/main/java/io/nuls/ledger/db/DataBaseArea.java
@@ -0,0 +1,6 @@
+package io.nuls.ledger.db;
+
+
+public interface DataBaseArea {
+    public String TB_LEDGER_ACCOUNT = "account";
+}
---
+++ ledger/nuls-ledger/src/main/java/io/nuls/ledger/rpc/cmd/TransactionCmd.java
@@ -0,0 +1,39 @@
+package io.nuls.ledger.rpc.cmd;
+
+import io.nuls.ledger.service.LedgerService;
+import io.nuls.rpc.cmd.BaseCmd;
+import io.nuls.rpc.model.CmdAnnotation;
+import io.nuls.rpc.model.CmdResponse;
+import io.nuls.tools.core.annotation.Autowired;
+import io.nuls.tools.core.annotation.Component;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.List;
+
+/**
+ * Created by wangkun23 on 2018/11/20.
+ */
+@Component
+public class TransactionCmd extends BaseCmd {
+
+    final Logger logger = LoggerFactory.getLogger(getClass());
+
+    @Autowired
+    private LedgerService ledgerService;
+
+    /**
+     * save uncomfirm transaction
+     *
+     * @param params
+     * @return
+     */
+    @CmdAnnotation(cmd = "lg_saveUncomfirmTx", version = 1.0, preCompatible = true)
+    public CmdResponse saveUncomfirmTx(List params) {
+        for (Object param : params) {
+            logger.info("param {}", param);
+        }
+        //TODO.. 验证参数个数和格式
+        return success("", "hash");
+    }
+}
