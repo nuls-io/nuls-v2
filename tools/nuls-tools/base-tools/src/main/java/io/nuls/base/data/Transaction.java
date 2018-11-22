8ac161e1a4b7e1c62a0fa81b1b332784289c2fca
Charlie <29900478@qq.com>
2018-11-22 17:09:58 +0800

    update

--- tools/nuls-tools/base-tools/src/main/java/io/nuls/base/data/Transaction.java
+++ tools/nuls-tools/base-tools/src/main/java/io/nuls/base/data/Transaction.java
@@ -155,6 +155,10 @@ public class Transaction extends BaseNulsData implements Cloneable {
         this.type = type;
     }
 
+    public byte[] getTxData() {
+        return txData;
+    }
+
     public long getTime() {
         return time;
     }
