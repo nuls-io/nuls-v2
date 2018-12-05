dc0144a666e4c65ea279e77d21abfcb9d8aff580
Naveen <naveen.balamuri@gmail.com>
2018-12-06 00:14:40 +0800

    evnt bus intial changes

---
+++ event-bus/nuls-event-bus/src/main/java/io/nuls/eventbus/constant/EBConstants.java
@@ -0,0 +1,16 @@
+package io.nuls.eventbus.constant;
+
+public class EBConstants {
+
+    public static String SUBSCRIBE = "subscribe";
+
+    public static String UNSUBSCRIBE = "unsubscribe";
+
+    public static String SEND = "send";
+
+    public static String EB_SUBSCRIBE = "eb_subscribe";
+
+    public static String EB_UNSUBSCRIBE = "eb_unsubscribe";
+
+    public static String EB_SEND = "eb_send";
+}
