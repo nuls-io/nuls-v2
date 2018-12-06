ae69f7d7032af8006ac83dc4210c16fac4bf673b
qinyifeng <qinyifeng@nuls.io>
2018-12-06 17:23:23 +0800

    fix:conflict

--- transaction/nuls-transaction/src/main/java/io/nuls/transaction/task/TxUnverifiedProcessTask.java
+++ transaction/nuls-transaction/src/main/java/io/nuls/transaction/task/TxUnverifiedProcessTask.java
@@ -11,15 +11,10 @@ import io.nuls.transaction.cache.TxVerifiedPool;
 import io.nuls.transaction.constant.TxConstant;
 import io.nuls.transaction.db.rocksdb.storage.TxUnverifiedStorageService;
 import io.nuls.transaction.db.rocksdb.storage.TxVerifiedStorageService;
-import io.nuls.transaction.model.bo.TxWrapper;
 import io.nuls.transaction.service.ConfirmedTransactionService;
 import io.nuls.transaction.utils.TransactionManager;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Iterator;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 /**
  * @author: Charlie
@@ -34,7 +29,7 @@ public class TxUnverifiedProcessTask implements Runnable {
     private ConfirmedTransactionService confirmedTransactionService = SpringLiteContext.getBean(ConfirmedTransactionService.class);
     private TxVerifiedStorageService txVerifiedStorageService = SpringLiteContext.getBean(TxVerifiedStorageService.class);
 
-    private List<TxWrapper> orphanTxList = new ArrayList<>();
+    private List<Transaction> orphanTxList = new ArrayList<>();
 
     //private static final int MAX_ORPHAN_SIZE = 200000;
 
@@ -56,39 +51,37 @@ public class TxUnverifiedProcessTask implements Runnable {
         System.out.println("count: " + count + " , size : " + size + " , orphan size : " + orphanTxList.size());
     }
 
-    private void doTask() {
+    private void doTask(){
         if (txVerifiedPool.getPoolSize() >= TxConstant.TX_UNVERIFIED_QUEUE_MAXSIZE) {
             return;
         }
 
-        TxWrapper txWrapper = null;
-        while ((txWrapper = txUnverifiedStorageService.pollTx()) != null && orphanTxList.size() < TxConstant.ORPHAN_CONTAINER_MAX_SIZE) {
+        Transaction tx = null;
+        while ((tx = txUnverifiedStorageService.pollTx()) != null && orphanTxList.size() < TxConstant.ORPHAN_CONTAINER_MAX_SIZE) {
             size++;
-            processTx(txWrapper, false);
+            processTx(tx, false);
         }
     }
 
-    private boolean processTx(TxWrapper txWrapper, boolean isOrphanTx) {
+    private boolean processTx(Transaction tx, boolean isOrphanTx){
         try {
-            int chainId = txWrapper.getChainId();
-            Transaction tx = txWrapper.getTx();
             Result result = transactionManager.verify(tx);
             if (result.isFailed()) {
                 return false;
             }
             //获取一笔交易(从已确认交易库中获取？)
-            Transaction transaction = confirmedTransactionService.getTransaction(chainId, tx.getHash());
-            if (null != transaction) {
+            Transaction transaction = confirmedTransactionService.getTransaction(tx.getHash());
+            if(null != transaction){
                 return isOrphanTx;
             }
             //todo 验证coinData
             Map<String, String> params = new HashMap<>();
             params.put("tx", tx.hex());
-            Response response = CmdDispatcher.requestAndResponse(ModuleE.LG.abbr, "verifyCoinData", params);
-            if (response.isSuccess()) {
-                txVerifiedPool.add(txWrapper, false);
+            Response response = CmdDispatcher.requestAndResponse(ModuleE.LG.abbr, "verifyCoinData",params);
+            if(response.isSuccess()){
+                txVerifiedPool.add(tx,false);
                 //保存到rocksdb
-                txVerifiedStorageService.putTx(txWrapper);
+                txVerifiedStorageService.putTx(tx);
                 //todo 保存到h2数据库
                 //todo 调账本记录未确认交易
                 //todo 转发
@@ -100,12 +93,12 @@ public class TxUnverifiedProcessTask implements Runnable {
     }
 
 
-    private void doOrphanTxTask() {
+    private void doOrphanTxTask(){
         //todo
         //时间排序TransactionTimeComparator
-        Iterator<TxWrapper> it = orphanTxList.iterator();
+        Iterator<Transaction> it = orphanTxList.iterator();
         while (it.hasNext()) {
-            TxWrapper tx = it.next();
+            Transaction tx = it.next();
             boolean success = processTx(tx, true);
             if (success) {
                 it.remove();
