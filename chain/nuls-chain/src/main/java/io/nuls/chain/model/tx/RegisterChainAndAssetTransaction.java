3645ab644ad2340d24a3cab5afa2b94176670c46
tangyi <tangyi@nuls.io>
2018-12-11 17:20:08 +0800

    chain_manager

--- chain/nuls-chain/src/main/java/io/nuls/chain/model/tx/RegisterChainAndAssetTransaction.java
+++ chain/nuls-chain/src/main/java/io/nuls/chain/model/tx/RegisterChainAndAssetTransaction.java
@@ -27,15 +27,11 @@ package io.nuls.chain.model.tx;
 import io.nuls.base.data.Transaction;
 
 /**
- * @program: nuls2.0
- * @description: 跨链注册交易
- * @author: lan
- * @create: 2018/11/19
- **/
+ * @author lan
+ * @date 2018/11/19
+ */
 public class RegisterChainAndAssetTransaction extends Transaction {
-
     public RegisterChainAndAssetTransaction() {
-        super();
+        super(BlockChainTxType.REGISTER_CHAIN_AND_ASSET.getKey());
     }
-
 }
