69bb09e2ae5ee69c7c66e6aad8e6cea097ff9454
wangko27 <wangkun@nuls.io>
2018-12-12 10:25:15 +0800

    bug string not charset

--- ledger/nuls-ledger/src/main/java/io/nuls/ledger/service/impl/AccountStateServiceImpl.java
+++ ledger/nuls-ledger/src/main/java/io/nuls/ledger/service/impl/AccountStateServiceImpl.java
@@ -32,6 +32,7 @@ import io.nuls.ledger.model.FreezeLockTimeState;
 import io.nuls.ledger.service.AccountStateService;
 import io.nuls.tools.core.annotation.Autowired;
 import io.nuls.tools.core.annotation.Service;
+import io.nuls.tools.crypto.HexUtil;
 import io.nuls.tools.thread.TimeService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -231,6 +232,6 @@ public class AccountStateServiceImpl implements AccountStateService {
      */
     private byte[] getKey(String address, int chainId, int assetId) {
         String key = address + "-" + chainId + "-" + assetId;
-        return key.getBytes();
+        return HexUtil.decode(key);
     }
 }
