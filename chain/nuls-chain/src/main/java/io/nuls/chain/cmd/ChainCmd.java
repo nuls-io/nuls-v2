3bcb816d9b129d4ea1abe8189aec631759b3b3a1
tangyi <tangyi@nuls.io>
2018-12-14 19:00:23 +0800

    chain_manager

--- chain/nuls-chain/src/main/java/io/nuls/chain/cmd/ChainCmd.java
+++ chain/nuls-chain/src/main/java/io/nuls/chain/cmd/ChainCmd.java
@@ -90,7 +90,7 @@ public class ChainCmd extends BaseChainCmd {
             asset.setChainId(blockChain.getChainId());
             asset.setSymbol((String) params.get("symbol"));
             asset.setName((String) params.get("name"));
-            asset.setDepositNuls(Integer.valueOf(CmConstants.PARAM_MAP.get(CmConstants.ASSET_DEPOSITNULS)));
+            asset.setDepositNuls(Integer.valueOf(CmConstants.PARAM_MAP.get(CmConstants.ASSET_DEPOSIT_NULS)));
             asset.setInitNumber(new BigInteger(params.get("initNumber").toString()));
             asset.setDecimalPlaces(Short.valueOf(params.get("decimalPlaces").toString()));
             asset.setAvailable(true);
