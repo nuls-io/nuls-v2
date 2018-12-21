f52ee7a79607981ef6a89083cec481033b25accf
qinyifeng <qinyifeng@nuls.io>
2018-12-21 15:01:24 +0800

    feat:tx call other module

--- transaction/nuls-transaction/src/main/java/io/nuls/transaction/rpc/call/AccountCall.java
+++ transaction/nuls-transaction/src/main/java/io/nuls/transaction/rpc/call/AccountCall.java
@@ -2,14 +2,11 @@ package io.nuls.transaction.rpc.call;
 
 import io.nuls.base.basic.AddressTool;
 import io.nuls.base.data.MultiSigAccount;
-import io.nuls.base.data.Transaction;
-import io.nuls.rpc.client.CmdDispatcher;
 import io.nuls.rpc.info.Constants;
-import io.nuls.rpc.model.message.Response;
-import io.nuls.tools.exception.NulsException;
-import io.nuls.transaction.model.bo.Chain;
+import io.nuls.rpc.model.ModuleE;
+import io.nuls.tools.log.Log;
+import io.nuls.tools.parse.JSONUtils;
 
-import java.math.BigInteger;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -17,20 +14,52 @@ import java.util.Map;
  * 调用其他模块跟交易相关的接口
  *
  * @author: qinyifeng
- * @date: 2018/12/05
+ * @date: 2018/12/20
  */
 public class AccountCall {
 
-    public static String getPrikey(String address, String password) throws NulsException {
-        //todo 查私钥;
+    public static String getPrikey(String address, String password) {
+        /**
+         * 查询地址私钥
+         * Query address private key
+         */
         int chainId = AddressTool.getChainIdByAddress(address);
-        return "";
+        Map<String, Object> params = new HashMap<>();
+        params.put(Constants.VERSION_KEY_STR, "1.0");
+        params.put("chainId", chainId);
+        params.put("address", address);
+        params.put("password", password);
+        try {
+            HashMap result = (HashMap) TransactionCall.request(ModuleE.AC.abbr, "ac_getPriKeyByAddress", params);
+            return (String) result.get("priKey");
+        } catch (Exception e) {
+            Log.error(e);
+        }
+        return null;
     }
 
-    public static MultiSigAccount getMultiSigAccount(byte[] multiSignAddress) throws NulsException {
+    /**
+     * 查询多签账户
+     * Query multi-sign account
+     *
+     * @param multiSignAddress
+     * @return
+     */
+    public static MultiSigAccount getMultiSigAccount(byte[] multiSignAddress) {
+        MultiSigAccount multiSigAccount = null;
         String address = AddressTool.getStringAddressByBytes(multiSignAddress);
-        //todo 获取多签账户
-        return new MultiSigAccount();
+        int chainId = AddressTool.getChainIdByAddress(address);
+        Map<String, Object> params = new HashMap<>();
+        params.put(Constants.VERSION_KEY_STR, "1.0");
+        params.put("chainId", chainId);
+        params.put("address", address);
+        try {
+            Object result = TransactionCall.request(ModuleE.AC.abbr, "ac_getMultiSigAccount", params);
+            multiSigAccount = JSONUtils.json2pojo(JSONUtils.obj2json(result), MultiSigAccount.class);
+        } catch (Exception e) {
+            Log.error(e);
+        }
+        return multiSigAccount;
     }
 
 }
--- transaction/nuls-transaction/src/main/java/io/nuls/transaction/rpc/call/LegerCall.java
+++ transaction/nuls-transaction/src/main/java/io/nuls/transaction/rpc/call/LegerCall.java
@@ -1,10 +1,14 @@
 package io.nuls.transaction.rpc.call;
 
+import io.nuls.base.basic.AddressTool;
 import io.nuls.base.data.Transaction;
 import io.nuls.rpc.client.CmdDispatcher;
 import io.nuls.rpc.info.Constants;
+import io.nuls.rpc.model.ModuleE;
 import io.nuls.rpc.model.message.Response;
+import io.nuls.tools.data.BigIntegerUtils;
 import io.nuls.tools.exception.NulsException;
+import io.nuls.tools.log.Log;
 import io.nuls.transaction.model.bo.Chain;
 import io.nuls.transaction.model.bo.TxRegister;
 
@@ -17,22 +21,38 @@ import java.util.Map;
  * 调用其他模块跟交易相关的接口
  *
  * @author: qinyifeng
- * @date: 2018/12/05
+ * @date: 2018/12/20
  */
 public class LegerCall {
 
-    public static boolean verifyCoinData(Chain chain, String txHex){
-        //todo 验证CoinData
-        /*try {
-
-            return true;
-        } catch (NulsException e){
-            chain.getLogger().info(e.getErrorCode().getMsg(), e.fillInStackTrace());
+    /**
+     * 验证CoinData
+     * @param chain
+     * @param txHex
+     * @return
+     */
+    public static boolean verifyCoinData(Chain chain, String txHex) {
+        Map<String, Object> params = new HashMap<>();
+        params.put(Constants.VERSION_KEY_STR, "1.0");
+        params.put("chainId", chain.getChainId());
+        params.put("txHex", txHex);
+        try {
+            //todo 验证CoinData
+            HashMap result = (HashMap) TransactionCall.request("lg_validateCoinData", ModuleE.LG.abbr, params);
+            return (Boolean) result.get("value");
+        } catch (Exception e) {
+            chain.getLogger().info(e.getMessage(), e.fillInStackTrace());
             return false;
-        }*/
-        return true;
+        }
     }
-    public static boolean verifyCoinData(Chain chain, Transaction tx){
+
+    /**
+     * 验证CoinData
+     * @param chain
+     * @param tx
+     * @return
+     */
+    public static boolean verifyCoinData(Chain chain, Transaction tx) {
         //todo 验证CoinData
         try {
             return verifyCoinData(chain, tx.hex());
@@ -42,15 +62,39 @@ public class LegerCall {
         }
     }
 
+    /**
+     * 查询nonce值
+     *
+     * @param address
+     * @param chainId
+     * @param assetId
+     * @return
+     * @throws NulsException
+     */
     public static byte[] getNonce(byte[] address, int chainId, int assetId) throws NulsException {
         //todo 查nonce
         byte[] nonce = new byte[]{'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'};
         return nonce;
     }
 
-    public static BigInteger getBalance(byte[] address, int chainId, int assetId) throws NulsException {
-        //todo 查余额;
-        return new BigInteger("10000");
+    /**
+     * 查询账户特定资产的余额
+     * Check the balance of an account-specific asset
+     */
+    public static BigInteger getBalance(byte[] address, int chainId, int assetId) {
+        String addressString = AddressTool.getStringAddressByBytes(address);
+        Map<String, Object> params = new HashMap<>();
+        params.put(Constants.VERSION_KEY_STR, "1.0");
+        params.put("chainId", chainId);
+        params.put("assetId", assetId);
+        params.put("address", addressString);
+        try {
+            HashMap result = (HashMap)TransactionCall.request(ModuleE.AC.abbr, "lg_getBalance", params);
+            return BigIntegerUtils.stringToBigInteger((String) result.get("available"));
+        } catch (Exception e) {
+            Log.error(e);
+        }
+        return new BigInteger("0");
     }
 
 }
