c907e05be21c76daf3d68b9fdc3b17476431f48c
ad19900913 <ad19900913@outlook.com>
2018-12-28 19:34:17 +0800

    update

--- tools/nuls-tools/rpc-tools/src/main/java/io/nuls/rpc/cmd/kernel/KernelCmd4Test.java
+++ tools/nuls-tools/rpc-tools/src/main/java/io/nuls/rpc/cmd/kernel/KernelCmd4Test.java
@@ -1,19 +1,15 @@
 /*
  *
  *  * MIT License
- *  *
  *  * Copyright (c) 2017-2018 nuls.io
- *  *
  *  * Permission is hereby granted, free of charge, to any person obtaining a copy
  *  * of this software and associated documentation files (the "Software"), to deal
  *  * in the Software without restriction, including without limitation the rights
  *  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  *  * copies of the Software, and to permit persons to whom the Software is
  *  * furnished to do so, subject to the following conditions:
- *  *
  *  * The above copyright notice and this permission notice shall be included in all
  *  * copies or substantial portions of the Software.
- *  *
  *  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  *  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  *  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -21,7 +17,6 @@
  *  * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
  *  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
  *  * SOFTWARE.
- *  *
  *
  */
 
@@ -71,13 +66,6 @@ public class KernelCmd4Test extends BaseCmd {
             Log.error(e);
             return failed(e.getMessage());
         }
-    }
-
-    @CmdAnnotation(cmd = "method1", version = 1.0, minEvent = 1,
-            description = "Test method1")
-    @Parameter(parameterName = "param1", parameterType = "string")
-    public Response method1(Map<String, Object> map) {
-        return success();
     }
 
 }
