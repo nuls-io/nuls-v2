ad400da04f956833957d32df967f1b651f781535
qinyifeng <qinyifeng@nuls.io>
2019-01-21 14:27:12 +0800

    fix:account

--- account/nuls-account/src/test/java/io/nuls/account/rpc/cmd/TransactionCmdTest.java
+++ account/nuls-account/src/test/java/io/nuls/account/rpc/cmd/TransactionCmdTest.java
@@ -1,30 +1,13 @@
 package io.nuls.account.rpc.cmd;
 
-import io.nuls.account.constant.AccountConstant;
-import io.nuls.account.constant.AccountErrorCode;
 import io.nuls.account.constant.RpcConstant;
-import io.nuls.account.model.bo.tx.AliasTransaction;
-import io.nuls.account.model.bo.tx.txdata.Alias;
-import io.nuls.account.model.dto.AccountKeyStoreDto;
-import io.nuls.account.model.dto.AccountOfflineDto;
 import io.nuls.account.model.dto.CoinDto;
-import io.nuls.account.model.dto.MulitpleAddressTransferDto;
-import io.nuls.account.model.dto.SimpleAccountDto;
-import io.nuls.base.basic.AddressTool;
-import io.nuls.base.data.CoinData;
-import io.nuls.base.data.CoinTo;
-import io.nuls.base.data.NulsDigestData;
-import io.nuls.base.data.Page;
+import io.nuls.account.model.dto.TransferDto;
 import io.nuls.rpc.client.CmdDispatcher;
-import io.nuls.rpc.info.Constants;
 import io.nuls.rpc.info.NoUse;
 import io.nuls.rpc.model.ModuleE;
 import io.nuls.rpc.model.message.Response;
-import io.nuls.tools.crypto.HexUtil;
-import io.nuls.tools.data.ByteUtils;
-import io.nuls.tools.exception.NulsRuntimeException;
 import io.nuls.tools.parse.JSONUtils;
-import io.nuls.tools.thread.TimeService;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
@@ -32,7 +15,6 @@ import java.math.BigInteger;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
@@ -62,10 +44,10 @@ public class TransactionCmdTest {
     }
 
     @Test
-    public void multipleAddressTransfer() throws Exception {
-        MulitpleAddressTransferDto transferDto = new MulitpleAddressTransferDto();
+    public void transfer() throws Exception {
+        TransferDto transferDto = new TransferDto();
         transferDto.setChainId(chainId);
-        transferDto.setRemark("multipleAddressTransfer test");
+        transferDto.setRemark("transfer test");
         List<CoinDto> inputs=new ArrayList<>();
         List<CoinDto> outputs=new ArrayList<>();
         CoinDto inputCoin1=new CoinDto();
@@ -73,7 +55,7 @@ public class TransactionCmdTest {
         inputCoin1.setPassword(password);
         inputCoin1.setAssetsChainId(chainId);
         inputCoin1.setAssetsId(1);
-        inputCoin1.setAmount(new BigInteger("1"));
+        inputCoin1.setAmount(new BigInteger("10000000"));
         inputs.add(inputCoin1);
 
         CoinDto outputCoin1=new CoinDto();
@@ -81,13 +63,13 @@ public class TransactionCmdTest {
         outputCoin1.setPassword(password);
         outputCoin1.setAssetsChainId(chainId);
         outputCoin1.setAssetsId(1);
-        outputCoin1.setAmount(new BigInteger("1"));
+        outputCoin1.setAmount(new BigInteger("10000000"));
         outputs.add(outputCoin1);
 
         transferDto.setInputs(inputs);
         transferDto.setOutputs(outputs);
-        Response cmdResp = CmdDispatcher.requestAndResponse(ModuleE.AC.abbr, "ac_multipleAddressTransfer", JSONUtils.json2map(JSONUtils.obj2json(transferDto)));
-        HashMap result = (HashMap) (((HashMap) cmdResp.getResponseData()).get("ac_multipleAddressTransfer"));
+        Response cmdResp = CmdDispatcher.requestAndResponse(ModuleE.AC.abbr, "ac_transfer", JSONUtils.json2map(JSONUtils.obj2json(transferDto)));
+        HashMap result = (HashMap) (((HashMap) cmdResp.getResponseData()).get("ac_transfer"));
         String txDigestHex = (String) result.get(RpcConstant.VALUE);
         System.out.println(txDigestHex);
     }
