c4665d373c4a749646876a08d075ba7dcbf2174b
qinyifeng <qinyifeng@nuls.io>
2019-02-20 17:10:32 +0800

    fix:askTx

--- account/nuls-account/src/test/java/io/nuls/account/rpc/cmd/TransactionCmdTest.java
+++ account/nuls-account/src/test/java/io/nuls/account/rpc/cmd/TransactionCmdTest.java
@@ -284,7 +284,7 @@ public class TransactionCmdTest {
         List<CoinDto> inputs = new ArrayList<>();
         List<CoinDto> outputs = new ArrayList<>();
         CoinDto inputCoin1 = new CoinDto();
-        inputCoin1.setAddress("5MR_2CVuGjQ3CYVkhFszxfSt6sodg1gDHYF");
+        inputCoin1.setAddress("5MR_2CfUsasd33vQV3HqGw6M3JwVsuVxJ7r");
         inputCoin1.setPassword(password);
         inputCoin1.setAssetsChainId(chainId);
         inputCoin1.setAssetsId(1);
