22d6ccbe06d4574a516f22085b9c79f1a64f237d
tanggang@nuls.io <123568TAG>
2019-02-26 18:34:29 +0800

    consensus commit

--- consensus/poc-consensus/src/main/java/io/nuls/poc/utils/manager/ChainManager.java
+++ consensus/poc-consensus/src/main/java/io/nuls/poc/utils/manager/ChainManager.java
@@ -90,7 +90,7 @@ public class ChainManager {
                     break;
                 }
                 try {
-                    Thread.sleep(200);
+                    Thread.sleep(1000);
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
