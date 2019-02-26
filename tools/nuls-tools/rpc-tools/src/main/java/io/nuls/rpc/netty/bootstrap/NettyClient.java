be81792d64c42792c0ce8b99433c5dd284270ec6
tanggang@nuls.io <123568TAG>
2019-02-26 11:57:53 +0800

    consensus commit

---
+++ tools/nuls-tools/rpc-tools/src/main/java/io/nuls/rpc/netty/bootstrap/NettyClient.java
@@ -0,0 +1,72 @@
+package io.nuls.rpc.netty.bootstrap;
+
+import io.netty.bootstrap.Bootstrap;
+import io.netty.channel.*;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+import io.netty.handler.codec.http.DefaultHttpHeaders;
+import io.netty.handler.codec.http.HttpClientCodec;
+import io.netty.handler.codec.http.HttpHeaders;
+import io.netty.handler.codec.http.HttpObjectAggregator;
+import io.netty.handler.codec.http.websocketx.WebSocketClientHandshaker;
+import io.netty.handler.codec.http.websocketx.WebSocketClientHandshakerFactory;
+import io.netty.handler.codec.http.websocketx.WebSocketVersion;
+import io.nuls.rpc.netty.handler.ClientHandler;
+import io.nuls.tools.log.Log;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+
+/**
+ * netty客服端启动实现类
+ * Customer Service Start Implementation Class
+ * @author tag
+ * 2019/2/21
+ * */
+public class NettyClient {
+    /**
+     * 连接服务器，返回连接通道
+     * Connect to the server and return to the connection channel
+     * */
+    public static SocketChannel createConnect(String uri){
+        try {
+            EventLoopGroup group=new NioEventLoopGroup();
+            Bootstrap boot=new Bootstrap();
+            boot.option(ChannelOption.SO_KEEPALIVE,true)
+                    .option(ChannelOption.TCP_NODELAY,true)
+                    .option(ChannelOption.SO_BACKLOG,1024*1024*10)
+                    .group(group)
+                    .channel(NioSocketChannel.class)
+                    .handler(new ChannelInitializer<SocketChannel>() {
+                        @Override
+                        protected void initChannel(SocketChannel socketChannel) throws Exception {
+                            ChannelPipeline p = socketChannel.pipeline();
+                            p.addLast(new ChannelHandler[]{new HttpClientCodec(),
+                                    new HttpObjectAggregator(1024*1024*10)});
+                            p.addLast("hookedHandler", new ClientHandler());
+                        }
+                    });
+            URI webSocketURI = null;
+            try {
+                webSocketURI = new URI(uri);
+            } catch (URISyntaxException e) {
+                e.printStackTrace();
+            }
+            HttpHeaders httpHeaders = new DefaultHttpHeaders();
+            //进行握手
+            WebSocketClientHandshaker handShaker = WebSocketClientHandshakerFactory.newHandshaker(webSocketURI, WebSocketVersion.V13, (String)null, true,httpHeaders);
+            final Channel channel=boot.connect(webSocketURI.getHost(),webSocketURI.getPort()).sync().channel();
+            ClientHandler handler = (ClientHandler)channel.pipeline().get("hookedHandler");
+            handler.setHandshaker(handShaker);
+            handShaker.handshake(channel);
+            //阻塞等待是否握手成功
+            handler.handshakeFuture().sync();
+            Log.info("与服务器："+webSocketURI.toString()+"握手成功");
+            return (SocketChannel) channel;
+        }catch (Exception e){
+            e.printStackTrace();
+            return null;
+        }
+    }
+}
---
+++ tools/nuls-tools/rpc-tools/src/main/java/io/nuls/rpc/netty/bootstrap/NettyServer.java
@@ -0,0 +1,129 @@
+package io.nuls.rpc.netty.bootstrap;
+
+import io.nuls.rpc.info.Constants;
+import io.nuls.rpc.info.HostInfo;
+import io.nuls.rpc.model.ModuleE;
+import io.nuls.rpc.netty.channel.manager.ConnectManager;
+import io.nuls.rpc.netty.thread.StartServerProcessor;
+import io.nuls.rpc.server.runtime.ServerRuntime;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * netty服务器端实现类
+ * Server-side implementation class
+ * @author tag
+ * 2019/2/21
+ * */
+public class NettyServer {
+
+    /**
+     * 根据预定义模块获得一个服务实例
+     * Get a server instance with predefined module
+     *
+     * @param moduleE 预定义模块 / Pre-defined module
+     * @return WsServer
+     */
+    public static NettyServer getInstance(ModuleE moduleE){
+        return getInstance(moduleE.abbr, moduleE.name, moduleE.domain);
+    }
+
+    /**
+     * 根据参数获得一个服务实例
+     * Get a server instance with Abbreviation & Name
+     *
+     * @param abbr   角色 / Role
+     * @param name   名称 / Name
+     * @param domain 域名 / Domian
+     * @return WsServer
+     */
+    public static NettyServer getInstance(String abbr, String name, String domain){
+        int port = HostInfo.randomPort();
+        startServer(port);
+        ConnectManager.LOCAL.setModuleAbbreviation(abbr);
+        ConnectManager.LOCAL.setModuleName(name);
+        ConnectManager.LOCAL.setModuleDomain(domain);
+        Map<String, String> connectionInformation = new HashMap<>(2);
+        connectionInformation.put(Constants.KEY_IP, HostInfo.getLocalIP());
+        connectionInformation.put(Constants.KEY_PORT, String.valueOf(port));
+        ConnectManager.LOCAL.setConnectionInformation(connectionInformation);
+        ConnectManager.LOCAL.setApiMethods(new ArrayList<>());
+        ConnectManager.LOCAL.setDependencies(new HashMap<>(8));
+        ConnectManager.LOCAL.setModuleRoles(new HashMap<>(1));
+        return new NettyServer();
+    }
+
+    /**
+     * 启动netty服务器，监听指定端口
+     * Start the netty server to listen on the specified port
+     * */
+    public static void startServer(int port){
+        Thread serverThread = new Thread(new StartServerProcessor(port));
+        serverThread.start();
+    }
+
+    /**
+     * 设置本模块的依赖角色
+     * Setting Dependent Roles for this Module
+     *
+     * @param key   依赖的角色 / Dependent roles
+     * @param value 依赖角色的版本 / Version of dependent roles
+     * @return WsServer
+     */
+    public NettyServer dependencies(String key, String value) {
+        ConnectManager.LOCAL.getDependencies().put(key, value);
+        return this;
+    }
+
+    /**
+     * 设置本模块的角色（角色名默认为模块编号）
+     * Setting up the role of this module(Role name defaults to module code)
+     *
+     * @param value 角色版本 / Version of role
+     * @return WsServer
+     */
+    public NettyServer moduleRoles(String[] value) {
+        ConnectManager.LOCAL.getModuleRoles().put(ServerRuntime.LOCAL.getModuleAbbreviation(), value);
+        return this;
+    }
+
+    /**
+     * 设置本模块的角色
+     * Setting up the role of this module
+     *
+     * @param key   角色 / Role
+     * @param value 角色版本 / Version of role
+     * @return WsServer
+     */
+    public NettyServer moduleRoles(String key, String[] value) {
+        ConnectManager.LOCAL.getModuleRoles().put(key, value);
+        return this;
+    }
+
+    /**
+     * 设置模块版本
+     * Set module version
+     *
+     * @param moduleVersion 模块的版本 / Version of module
+     * @return WsServer
+     */
+    public NettyServer moduleVersion(String moduleVersion) {
+        ConnectManager.LOCAL.setModuleVersion(moduleVersion);
+        return this;
+    }
+
+    /**
+     * 扫描指定路径，得到所有接口的详细信息
+     * Scan the specified path for details of all interfaces
+     *
+     * @param scanPackage 需要扫描的包路径 / Packet paths to be scanned
+     * @return WsServer
+     * @throws Exception 找到重复命令(cmd) / Find duplicate commands (cmd)
+     */
+    public NettyServer scanPackage(String scanPackage) throws Exception {
+        ConnectManager.scanPackage(scanPackage);
+        return this;
+    }
+}
