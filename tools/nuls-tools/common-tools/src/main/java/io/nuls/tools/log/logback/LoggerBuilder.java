1a0ca50281518f8676bff6e721f18639c2c7a272
zhouwei1985 <251556768@qq.com>
2019-03-01 18:08:47 +0800

    logger

--- tools/nuls-tools/common-tools/src/main/java/io/nuls/tools/log/logback/LoggerBuilder.java
+++ tools/nuls-tools/common-tools/src/main/java/io/nuls/tools/log/logback/LoggerBuilder.java
@@ -16,50 +16,58 @@ import java.util.Map;
  *
  * @author tag
  * 2018/12/17
- * */
+ */
 public class LoggerBuilder {
 
     private static final Map<String, NulsLogger> container = new HashMap<>();
     private static final Level DEFAULT_LEVEL = Level.ALL;
 
-    public static NulsLogger getLogger(String folderName,String fileName) {
-        String realKey = folderName+"/"+fileName;
+    static {
+        LoggerContext context = (LoggerContext) LoggerFactory.getILoggerFactory();
+        Logger logger = context.getLogger("io.netty");
+        logger.setAdditive(false);
+        logger.setLevel(Level.INFO);
+    }
+
+    public static NulsLogger getLogger(String folderName, String fileName) {
+        String realKey = folderName + "/" + fileName;
         return getLogger(realKey);
     }
 
 
-    public static NulsLogger getLogger(String folderName,String fileName,Level level) {
-        String realKey = folderName+"/"+fileName;
-        return getLogger(realKey,level,level);
+    public static NulsLogger getLogger(String folderName, String fileName, Level level) {
+        String realKey = folderName + "/" + fileName;
+        return getLogger(realKey, level, level);
     }
 
-    public static NulsLogger getLogger(String folderName,String fileName,Level fileLevel,Level consoleLevel){
-        String realKey = folderName+"/"+fileName;
-        return getLogger(realKey,fileLevel,consoleLevel);
+    public static NulsLogger getLogger(String folderName, String fileName, Level fileLevel, Level consoleLevel) {
+        String realKey = folderName + "/" + fileName;
+        return getLogger(realKey, fileLevel, consoleLevel);
     }
+
     public static NulsLogger getLogger(String fileName) {
-        return getLogger(fileName,DEFAULT_LEVEL,DEFAULT_LEVEL);
+        return getLogger(fileName, DEFAULT_LEVEL, DEFAULT_LEVEL);
     }
 
-    public static NulsLogger getLogger(String fileName,Level level) {
-        return getLogger(fileName,level,level);
+    public static NulsLogger getLogger(String fileName, Level level) {
+        return getLogger(fileName, level, level);
     }
 
-    public static NulsLogger getLogger(String fileName,Level fileLevel,Level consoleLevel) {
+    public static NulsLogger getLogger(String fileName, Level fileLevel, Level consoleLevel) {
         NulsLogger logger = container.get(fileName);
-        if(logger != null) {
+        if (logger != null) {
             return logger;
         }
         synchronized (LoggerBuilder.class) {
-            logger = build(fileName,fileLevel,consoleLevel);
-            container.put(fileName,logger);
+            logger = build(fileName, fileLevel, consoleLevel);
+            container.put(fileName, logger);
         }
         return logger;
     }
 
     @SuppressWarnings("unchecked")
-    private static NulsLogger build(String fileName, Level fileLevel,Level consoleLevel) {
-        RollingFileAppender fileAppender = LogAppender.getAppender(fileName,fileLevel);
+    private static NulsLogger build(String fileName, Level fileLevel, Level consoleLevel) {
+        RollingFileAppender fileAppender = LogAppender.getAppender(fileName, fileLevel);
         LoggerContext context = (LoggerContext) LoggerFactory.getILoggerFactory();
         Logger logger = context.getLogger(fileAppender.getEncoder().toString());
         //设置不向上级打印信息
