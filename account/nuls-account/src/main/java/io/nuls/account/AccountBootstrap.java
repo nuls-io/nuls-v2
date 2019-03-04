a71daa7d8a461c74e3e5fea92fc7536f53bda55a
lanjinsheng <lanjinsheng@idata365.com>
2019-03-04 17:37:13 +0800

    change module start logic

--- account/nuls-account/src/main/java/io/nuls/account/AccountBootstrap.java
+++ account/nuls-account/src/main/java/io/nuls/account/AccountBootstrap.java
@@ -8,8 +8,10 @@ import io.nuls.account.constant.AccountStorageConstant;
 import io.nuls.account.util.manager.ChainManager;
 import io.nuls.db.constant.DBErrorCode;
 import io.nuls.db.service.RocksDBService;
+import io.nuls.rpc.info.HostInfo;
 import io.nuls.rpc.model.ModuleE;
 import io.nuls.rpc.modulebootstrap.Module;
+import io.nuls.rpc.modulebootstrap.NulsRpcModuleBootstrap;
 import io.nuls.rpc.modulebootstrap.RpcModule;
 import io.nuls.rpc.modulebootstrap.RpcModuleState;
 import io.nuls.tools.core.ioc.SpringLiteContext;
@@ -25,21 +27,29 @@ import java.io.IOException;
  * @author: qinyifeng
  * @date: 2018/10/15
  */
-public class AccountBootstrap  extends RpcModule {
+public class AccountBootstrap extends RpcModule {
+    public static void main(String[] args) {
+        if (args == null || args.length == 0) {
+            args = new String[]{HostInfo.getLocalIP() + ":8887"};
+        }
+        NulsRpcModuleBootstrap.run("io.nuls", args);
+    }
 
     /**
      * 返回此模块的依赖模块
      * 可写作 return new Module[]{new Module(ModuleE.LG.abbr, "1.0"),new Module(ModuleE.TX.abbr, "1.0")}
+     *
      * @return
      */
     @Override
     public Module[] getDependencies() {
-        return new Module[]{ new Module(ModuleE.LG.abbr, "1.0"),
+        return new Module[]{new Module(ModuleE.LG.abbr, "1.0"),
                 new Module(ModuleE.TX.abbr, "1.0")};
     }
 
     /**
      * 返回当前模块的描述信息
+     *
      * @return
      */
     @Override
@@ -62,6 +72,7 @@ public class AccountBootstrap  extends RpcModule {
 
     /**
      * 已完成spring init注入，开始启动模块
+     *
      * @return 如果启动完成返回true，模块将进入ready状态，若启动失败返回false，10秒后会再次调用此方法
      */
     @Override
@@ -70,7 +81,7 @@ public class AccountBootstrap  extends RpcModule {
         try {
             //初始化数据库
             initDB();
-        }catch (Exception e){
+        } catch (Exception e) {
             return false;
         }
         return true;
@@ -78,6 +89,7 @@ public class AccountBootstrap  extends RpcModule {
 
     /**
      * 所有外部依赖进入ready状态后会调用此方法，正常启动后返回Running状态
+     *
      * @return
      */
     @Override
@@ -91,6 +103,7 @@ public class AccountBootstrap  extends RpcModule {
 
     /**
      * 某个外部依赖连接丢失后，会调用此方法，可控制模块状态，如果返回Ready,则表明模块退化到Ready状态，当依赖重新准备完毕后，将重新触发onDependenciesReady方法，若返回的状态是Running，将不会重新触发onDependenciesReady
+     *
      * @param module
      * @return
      */
@@ -98,6 +111,7 @@ public class AccountBootstrap  extends RpcModule {
     public RpcModuleState onDependenciesLoss(Module module) {
         return RpcModuleState.Ready;
     }
+
     public static void initCfg() {
         try {
             NulsConfig.MODULES_CONFIG = ConfigLoader.loadIni(NulsConfig.MODULES_CONFIG_FILE);
@@ -111,7 +125,7 @@ public class AccountBootstrap  extends RpcModule {
                 NulsConfig.DEFAULT_ENCODING = NulsConfig.MODULES_CONFIG.getCfgValue(AccountConstant.CFG_SYSTEM_SECTION, AccountConstant.CFG_SYSTEM_DEFAULT_ENCODING);
                 //set system language
                 String language = NulsConfig.MODULES_CONFIG.getCfgValue(AccountConstant.CFG_SYSTEM_SECTION, AccountConstant.CFG_SYSTEM_LANGUAGE);
-                I18nUtils.loadLanguage(AccountBootstrap.class,"languages", language);
+                I18nUtils.loadLanguage(AccountBootstrap.class, "languages", language);
                 I18nUtils.setLanguage(language);
                 //ACCOUNTKEYSTORE_FOLDER_NAME
                 String keystoreFolder = NulsConfig.MODULES_CONFIG.getCfgValue(AccountConstant.CFG_SYSTEM_SECTION, AccountConstant.CFG_SYSTEM_TKEYSTORE_FOLDER);
