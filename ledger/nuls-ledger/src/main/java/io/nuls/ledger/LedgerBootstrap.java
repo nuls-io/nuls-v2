d22558f5389cc6c148d8c858f08d3bf8f8b9171b
lijun.zhou <zhouj.172@gmail.com>
2019-03-04 18:39:13 +0800

    模块启动改造

--- ledger/nuls-ledger/src/main/java/io/nuls/ledger/LedgerBootstrap.java
+++ ledger/nuls-ledger/src/main/java/io/nuls/ledger/LedgerBootstrap.java
@@ -53,7 +53,7 @@ public class LedgerBootstrap extends RpcModule {
 
     public static void main(String[] args) {
         if (args == null || args.length == 0) {
-            args = new String[]{HostInfo.getLocalIP() + ":8887"};
+            args = new String[]{HostInfo.getLocalIP() + ":8887/ws"};
         }
         NulsRpcModuleBootstrap.run("io.nuls.ledger", args);
     }
