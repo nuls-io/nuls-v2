37a6166f997f2338e182d9b4a26d7b2ac2ace0a3
lanjinsheng <lanjinsheng@idata365.com>
2019-03-04 15:20:50 +0800

    fix bugs

--- account/nuls-account/src/main/java/io/nuls/account/AccountModule.java
+++ account/nuls-account/src/main/java/io/nuls/account/AccountModule.java
@@ -98,7 +98,7 @@ public class AccountModule extends RpcModule {
                 NulsConfig.DEFAULT_ENCODING = NulsConfig.MODULES_CONFIG.getCfgValue(AccountConstant.CFG_SYSTEM_SECTION, AccountConstant.CFG_SYSTEM_DEFAULT_ENCODING);
                 //set system language
                 String language = NulsConfig.MODULES_CONFIG.getCfgValue(AccountConstant.CFG_SYSTEM_SECTION, AccountConstant.CFG_SYSTEM_LANGUAGE);
-                I18nUtils.loadLanguage("languages", language);
+                I18nUtils.loadLanguage(AccountModule.class,"languages", language);
                 I18nUtils.setLanguage(language);
                 //ACCOUNTKEYSTORE_FOLDER_NAME
                 String keystoreFolder = NulsConfig.MODULES_CONFIG.getCfgValue(AccountConstant.CFG_SYSTEM_SECTION, AccountConstant.CFG_SYSTEM_TKEYSTORE_FOLDER);
