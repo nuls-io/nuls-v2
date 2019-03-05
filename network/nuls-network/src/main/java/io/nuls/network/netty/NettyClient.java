3670cc3efbdec9d411ba8b08ae7c8fe826910610
lanjinsheng <lanjinsheng@idata365.com>
2019-03-05 14:57:23 +0800

    modify netty

--- network/nuls-network/src/main/java/io/nuls/network/netty/NettyClient.java
+++ network/nuls-network/src/main/java/io/nuls/network/netty/NettyClient.java
@@ -26,8 +26,9 @@
 package io.nuls.network.netty;
 
 import io.netty.bootstrap.Bootstrap;
+import io.netty.buffer.PooledByteBufAllocator;
+import io.netty.channel.AdaptiveRecvByteBufAllocator;
 import io.netty.channel.ChannelFuture;
-import io.netty.channel.ChannelFutureListener;
 import io.netty.channel.ChannelOption;
 import io.netty.channel.EventLoopGroup;
 import io.netty.channel.nio.NioEventLoopGroup;
@@ -36,7 +37,6 @@ import io.netty.channel.socket.nio.NioSocketChannel;
 import io.netty.util.AttributeKey;
 import io.nuls.network.model.Node;
 import io.nuls.network.netty.handler.ClientChannelHandler;
-import io.nuls.tools.thread.TimeService;
 
 import static io.nuls.network.constant.NetworkConstant.CONNETCI_TIME_OUT;
 import static io.nuls.network.utils.LoggerUtil.Log;
@@ -78,8 +78,9 @@ public class NettyClient {
                 .option(ChannelOption.SO_KEEPALIVE, true)
                 .option(ChannelOption.SO_SNDBUF, 128 * 1024)
                 .option(ChannelOption.SO_RCVBUF, 128 * 1024)
-//                .option(ChannelOption.RCVBUF_ALLOCATOR, AdaptiveRecvByteBufAllocator.DEFAULT)
-//                .option(ChannelOption.ALLOCATOR, PooledByteBufAllocator.DEFAULT)
+                .option(ChannelOption.RCVBUF_ALLOCATOR, AdaptiveRecvByteBufAllocator.DEFAULT)
+                .option(ChannelOption.ALLOCATOR, PooledByteBufAllocator.DEFAULT)
+                .option(ChannelOption.CONNECT_TIMEOUT_MILLIS, CONNETCI_TIME_OUT)
                 .option(ChannelOption.CONNECT_TIMEOUT_MILLIS, CONNETCI_TIME_OUT)
                 .handler(new NulsChannelInitializer<>(new ClientChannelHandler()));
     }
