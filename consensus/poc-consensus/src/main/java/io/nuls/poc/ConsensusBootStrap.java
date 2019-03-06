23e215514d5312e1662f4d46abc75a2f3d940066
tanggang@nuls.io <123568TAG>
2019-03-06 17:28:09 +0800

    consensus update

--- consensus/poc-consensus/src/main/java/io/nuls/poc/ConsensusBootStrap.java
+++ consensus/poc-consensus/src/main/java/io/nuls/poc/ConsensusBootStrap.java
@@ -6,9 +6,11 @@ import io.nuls.poc.storage.LanguageService;
 import io.nuls.poc.utils.manager.ChainManager;
 import io.nuls.rpc.info.HostInfo;
 import io.nuls.rpc.model.ModuleE;
-import io.nuls.rpc.netty.bootstrap.NettyServer;
-import io.nuls.rpc.netty.channel.manager.ConnectManager;
-import io.nuls.rpc.netty.processor.ResponseMessageProcessor;
+import io.nuls.rpc.modulebootstrap.Module;
+import io.nuls.rpc.modulebootstrap.NulsRpcModuleBootstrap;
+import io.nuls.rpc.modulebootstrap.RpcModule;
+import io.nuls.rpc.modulebootstrap.RpcModuleState;
+import io.nuls.tools.core.annotation.Component;
 import io.nuls.tools.core.ioc.SpringLiteContext;
 import io.nuls.tools.log.Log;
 import io.nuls.tools.parse.ConfigLoader;
@@ -19,35 +21,85 @@ import java.nio.charset.Charset;
 import java.util.Properties;
 
 import static java.nio.charset.StandardCharsets.UTF_8;
-
 /**
  * 共识模块启动及初始化管理
  * Consensus Module Startup and Initialization Management
  *
  * @author tag
- * 2018/11/7
+ * 2018/3/4
  */
-public class ConsensusBootStrap {
+@Component
+public class ConsensusBootStrap extends RpcModule {
+
+    public static void main(String[] args){
+        if (args == null || args.length == 0) {
+            args = new String[]{HostInfo.getLocalIP() + ":8887/ws"};
+        }
+        NulsRpcModuleBootstrap.run(ConsensusConstant.BOOT_PATH,args);
+    }
     /**
-     * 共识模块启动方法
-     * Consensus module startup method
+     * 初始化模块，比如初始化RockDB等，在此处初始化后，可在其他bean的afterPropertiesSet中使用
+     * 在onStart前会调用此方法
+     *
      */
-    public static void main(String[] args) {
+    @Override
+    public void init() {
         try {
             initSys();
             initDB();
-            SpringLiteContext.init(ConsensusConstant.CONTEXT_PATH);
             initLanguage();
-            initServer();
-            while (!ConnectManager.isReady()) {
+        }catch (Exception e){
+            Log.error(e);
+        }
+    }
+
+    /**
+     * 指定RpcCmd的包名
+     * 可以不实现此方法，若不实现将使用spring init扫描的包
+     * @return
+     */
+    @Override
+    public String getRpcCmdPackage(){
+        return ConsensusConstant.RPC_PATH;
+    }
+
+    @Override
+    public Module[] getDependencies() {
+        return new Module[]{
+                new Module(ModuleE.BL.abbr, "1.0"),
+                new Module(ModuleE.AC.abbr, "1.0"),
+                new Module(ModuleE.TX.abbr, "1.0")};
+    }
+
+    @Override
+    public Module moduleInfo() {
+        return new Module(ModuleE.CS.abbr,"1.0");
+    }
+
+    @Override
+    public boolean doStart() {
+        try {
+            while (!isDependencieReady()){
                 Log.debug("wait depend modules ready");
                 Thread.sleep(2000L);
             }
             SpringLiteContext.getBean(ChainManager.class).runChain();
-        } catch (Exception e) {
-            Log.error("consensus startup error！");
+            return true;
+        }catch (Exception e){
             Log.error(e);
+            return false;
         }
+    }
+
+    @Override
+    public RpcModuleState onDependenciesReady() {
+        Log.debug("cs onDependenciesReady");
+        return RpcModuleState.Running;
+    }
+
+    @Override
+    public RpcModuleState onDependenciesLoss(Module dependenciesModule) {
+        return RpcModuleState.Ready;
     }
 
     /**
@@ -69,7 +121,8 @@ public class ConsensusBootStrap {
         Properties properties = ConfigLoader.loadProperties(ConsensusConstant.DB_CONFIG_NAME);
         String path = properties.getProperty(ConsensusConstant.DB_DATA_PATH, ConsensusConstant.DB_DATA_DEFAULT_PATH);
         RocksDBService.init(path);
-
+        RocksDBService.createTable(ConsensusConstant.DB_NAME_CONSUME_LANGUAGE);
+        RocksDBService.createTable(ConsensusConstant.DB_NAME_CONSUME_CONGIF);
     }
 
     /**
@@ -84,36 +137,6 @@ public class ConsensusBootStrap {
         I18nUtils.setLanguage(language);
         if (null == languageDB) {
             languageService.saveLanguage(language);
-        }
-    }
-
-    /**
-     * 共识模块启动WebSocket服务，用于其他模块连接共识模块与共识模块交互
-     */
-    private static void initServer() {
-        try {
-            try {
-                NettyServer.getInstance(ModuleE.CS)
-                        .moduleRoles(new String[]{"1.0"})
-                        .moduleVersion("1.0")
-                        .dependencies(ModuleE.BL.abbr, "1.0")
-                        .dependencies(ModuleE.TX.abbr, "1.0")
-                        .scanPackage("io.nuls.poc.rpc");
-                String kernelUrl = "ws://"+ HostInfo.getLocalIP()+":8887/ws";
-                /*
-                * 链接到指定地址
-                * */
-                ConnectManager.getConnectByUrl(kernelUrl);
-                /*
-                * 和指定地址同步
-                * */
-                ResponseMessageProcessor.syncKernel(kernelUrl);
-            } catch (Exception e) {
-                Log.error("Account initServer failed", e);
-            }
-        } catch (Exception e) {
-            Log.error("Consensus startup webSocket server error!");
-            e.printStackTrace();
         }
     }
 }
