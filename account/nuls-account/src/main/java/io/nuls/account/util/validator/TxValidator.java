50a7bef36e19bf5fa73293ad998b9203952475a3
Charlie <29900478@qq.com>
2019-03-16 16:38:57 +0800

    update

--- account/nuls-account/src/main/java/io/nuls/account/util/validator/TxValidator.java
+++ account/nuls-account/src/main/java/io/nuls/account/util/validator/TxValidator.java
@@ -220,7 +220,7 @@ public class TxValidator {
             int assetsChainId = coinFrom.getAssetsChainId();
             int assetsId = coinFrom.getAssetsId();
             //黑洞地址不能发起转账
-            if(!Arrays.equals(AccountConstant.BLACK_HOLE_ADDRESS, coinFrom.getAddress())){
+            if(Arrays.equals(AccountConstant.BLACK_HOLE_ADDRESS, coinFrom.getAddress())){
                 throw new NulsException(AccountErrorCode.ADDRESS_TRANSFER_BAN);
             }
             // 发送方from中地址和资产对应的链id必须发起链id
