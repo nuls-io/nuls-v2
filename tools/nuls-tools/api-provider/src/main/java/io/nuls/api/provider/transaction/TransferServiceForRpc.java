599f4cd0119219630216c95d51c3a4eea4ddca43
lijun.zhou <zhouj.172@gmail.com>
2019-03-21 17:04:21 +0800

    集成测试模块

--- tools/nuls-tools/api-provider/src/main/java/io/nuls/api/provider/transaction/TransferServiceForRpc.java
+++ tools/nuls-tools/api-provider/src/main/java/io/nuls/api/provider/transaction/TransferServiceForRpc.java
@@ -4,21 +4,21 @@ import io.nuls.api.provider.BaseReq;
 import io.nuls.api.provider.BaseRpcService;
 import io.nuls.api.provider.Provider;
 import io.nuls.api.provider.Result;
-import io.nuls.api.provider.transaction.facade.GetConfirmedTxByHashReq;
-import io.nuls.api.provider.transaction.facade.GetTxByHashReq;
-import io.nuls.api.provider.transaction.facade.TransferByAliasReq;
-import io.nuls.api.provider.transaction.facade.TransferReq;
+import io.nuls.api.provider.transaction.facade.*;
 import io.nuls.base.basic.NulsByteBuffer;
 import io.nuls.base.constant.TxStatusEnum;
+import io.nuls.base.data.Coin;
 import io.nuls.base.data.Transaction;
 import io.nuls.rpc.model.ModuleE;
 import io.nuls.tools.crypto.HexUtil;
 import io.nuls.tools.exception.NulsException;
+import io.nuls.tools.model.DateUtils;
 import io.nuls.tools.model.StringUtils;
 import lombok.extern.slf4j.Slf4j;
 
 import java.util.Map;
 import java.util.function.Function;
+import java.util.stream.Collectors;
 
 /**
  * @Author: zhoulijun
@@ -51,6 +51,12 @@ public class TransferServiceForRpc extends BaseRpcService implements TransferSer
     }
 
     @Override
+    public Result<TransactionData> getSimpleTxDataByHash(GetConfirmedTxByHashReq req) {
+        Function<Map,Result> callback = res->tranderTransactionData(tranderTransaction(res));
+        return callRpc(ModuleE.TX.abbr,"tx_getConfirmedTxClient",req,callback);
+    }
+
+    @Override
     protected  <T,R> Result<T> call(String method, Object req, Function<R,Result> res){
         return callRpc(ModuleE.AC.abbr,method,req,res);
     }
@@ -62,22 +68,57 @@ public class TransferServiceForRpc extends BaseRpcService implements TransferSer
         return callRpc(ModuleE.TX.abbr,method,req,callback);
     }
 
-    private Result<Transaction> tranderTransaction(Map<String,Object> res){
+    private Result<Transaction> tranderTransaction(Map<String,Object> data){
         try {
-            String hexString = (String) res.get("txHex");
+            String hexString = (String) data.get("txHex");
             if(hexString == null){
                 return fail(ERROR_CODE,"not found tx");
             }
             Transaction transaction = new Transaction();
             transaction.parse(new NulsByteBuffer(HexUtil.decode(hexString)));
-            transaction.setBlockHeight(Long.parseLong(String.valueOf(res.get("height"))));
-            Integer state = (Integer) res.get("status");
+            transaction.setBlockHeight(Long.parseLong(String.valueOf(data.get("height"))));
+            Integer state = (Integer) data.get("status");
             transaction.setStatus(state == TxStatusEnum.UNCONFIRM.getStatus() ? TxStatusEnum.UNCONFIRM : TxStatusEnum.CONFIRMED);
             return success(transaction);
         } catch (NulsException e) {
             log.error("反序列化transaction发生异常",e);
             return fail(ERROR_CODE);
         }
+    }
+
+    private Result<TransactionData> tranderTransactionData(Result<Transaction>  data){
+        try {
+            Transaction transaction = data.getData();
+            TransactionData res = new TransactionData();
+            res.setBlockHeight(transaction.getBlockHeight());
+            res.setStatus(transaction.getStatus());
+            res.setHash(transaction.getHash().toString());
+            res.setRemark(HexUtil.encode(transaction.getRemark()));
+            res.setInBlockIndex(transaction.getInBlockIndex());
+            res.setSize(transaction.getSize());
+            res.setTime(DateUtils.timeStamp2DateStr(transaction.getTime()));
+            res.setTransactionSignature(HexUtil.encode(transaction.getTransactionSignature()));
+            res.setType(transaction.getType());
+            res.setForm(transaction.getCoinDataInstance().getFrom().stream().map(coinData->{
+                TransactionCoinData tcd = buildTransactionCoinData(coinData);
+                tcd.setNonce(HexUtil.encode(coinData.getNonce()));
+                return tcd;
+            }).collect(Collectors.toList()));
+            res.setTo(transaction.getCoinDataInstance().getTo().stream().map(this::buildTransactionCoinData).collect(Collectors.toList()));
+            return success(res);
+        } catch (NulsException e) {
+            log.error("反序列化transaction发生异常",e);
+            return fail(ERROR_CODE);
+        }
+    }
+
+    private TransactionCoinData buildTransactionCoinData(Coin coinData){
+        TransactionCoinData tcd = new TransactionCoinData();
+        tcd.setAddress(HexUtil.encode(coinData.getAddress()));
+        tcd.setAmount(coinData.getAmount());
+        tcd.setAssetsChainId(coinData.getAssetsChainId());
+        tcd.setAssetsId(coinData.getAssetsId());
+        return tcd;
     }
 
 }
