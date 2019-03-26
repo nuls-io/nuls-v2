eaf263aa9280b225fb8bd247ad60764320f2f226
lanjinsheng <lanjinsheng@idata365.com>
2019-03-26 12:50:30 +0800

    modify logs

--- network/nuls-network/src/main/java/io/nuls/network/utils/LoggerUtil.java
+++ network/nuls-network/src/main/java/io/nuls/network/utils/LoggerUtil.java
@@ -48,6 +48,7 @@ public class LoggerUtil {
     public static final String LOGGER_KEY3 = ModuleE.TX.abbr;
     public static final String LOGGER_KEY4 = ModuleE.CS.abbr;
     public static NulsLogger Log = LoggerBuilder.getLogger("nwLogs", "nw", Level.ALL);
+    public static NulsLogger NwInfosLog = LoggerBuilder.getLogger("nwLogs", "nwInfos", Level.ALL);
     public static Map<String, NulsLogger> logMap = new HashMap<>();
 
     public static void createLogs(int chainId) {
