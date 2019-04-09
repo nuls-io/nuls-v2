3a0a7edadd1100528fb991b21f86c980bb4a2776
ad19900913 <ad19900913@outlook.com>
2019-04-09 09:53:53 +0800

    refactor: 1.spring lite context refactor

--- tools/nuls-tools/common-tools/src/main/java/io/nuls/tools/protocol/MessageHandler.java
+++ tools/nuls-tools/common-tools/src/main/java/io/nuls/tools/protocol/MessageHandler.java
@@ -23,10 +23,7 @@
  */
 package io.nuls.tools.protocol;
 
-import java.lang.annotation.ElementType;
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
-import java.lang.annotation.Target;
+import java.lang.annotation.*;
 
 /**
  * Created by zhouwei on 2017/10/26.
