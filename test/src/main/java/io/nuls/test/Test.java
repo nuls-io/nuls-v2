db1c322ee486478a0cd81758a361d83c80eb59ad
lijun.zhou <zhouj.172@gmail.com>
2019-04-10 15:33:14 +0800

    nulstar对接脚本修改

--- test/src/main/java/io/nuls/test/Test.java
+++ test/src/main/java/io/nuls/test/Test.java
@@ -1,6 +1,7 @@
 package io.nuls.test;
 
 import io.nuls.api.provider.account.facade.AccountInfo;
+import io.nuls.test.cases.SleepAdapter;
 import io.nuls.test.cases.account.GetAccountByAddressCase;
 import io.nuls.test.controller.RemoteCaseReq;
 import io.nuls.test.controller.RemoteResult;
@@ -16,7 +17,7 @@ public class Test {
 
     public static void main(String[] args) {
         RemoteCaseReq req = new RemoteCaseReq();
-        req.setCaseClass(GetAccountByAddressCase.class);
+        req.setCaseClass(SleepAdapter.$15SEC.class);
         req.setParam("5MR_2CaLdKkCgdLAg9NYnppSRU9o5Lkx9wT");
         RestFulUtils.getInstance().setServerUri("http://192.168.1.115:9999/api");
         RemoteResult<AccountInfo> res = RestFulUtils.getInstance().post("/remote/call", MapUtils.beanToMap(req));
