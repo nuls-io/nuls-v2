c3504d702223403df9e1ec4559642d0ef5455249
lijun.zhou <zhouj.172@gmail.com>
2019-04-25 17:15:06 +0800

    集成测试

--- test/src/main/java/io/nuls/test/cases/transcation/batch/BatchCreateTransferCase.java
+++ test/src/main/java/io/nuls/test/cases/transcation/batch/BatchCreateTransferCase.java
@@ -13,6 +13,8 @@ import io.nuls.tools.log.Log;
 import io.nuls.tools.thread.ThreadUtils;
 
 import java.math.BigInteger;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.atomic.AtomicInteger;
 
 import static io.nuls.test.cases.Constants.REMARK;
 
@@ -35,28 +37,40 @@ public class BatchCreateTransferCase extends BaseTranscationCase<Boolean, Intege
 
     @Override
     public Boolean doTest(Integer count, int depth) throws TestFailException {
-        ThreadUtils.createAndRunThread("batch-transfer", () -> {
-            int i = 0;
-            int successTotal = 0;
+        ThreadUtils.createAndRunThread("batch-start",()->{
+            AtomicInteger doneTotal = new AtomicInteger(0);
+            AtomicInteger successTotal = new AtomicInteger(0);
+            CountDownLatch latch = new CountDownLatch(5);
             Long start = System.currentTimeMillis();
-            while (i < count) {
-                int index = i % batchCreateAccountCase.getFormList().size();
-                String formAddress = batchCreateAccountCase.getFormList().get(index);
-                String toAddress = batchCreateAccountCase.getToList().get(index);
-                i++;
-                TransferReq.TransferReqBuilder builder =
-                        new TransferReq.TransferReqBuilder(config.getChainId(), config.getAssetsId())
-                                .addForm(formAddress, Constants.PASSWORD, TRANSFER_AMOUNT)
-                                .addTo(toAddress, TRANSFER_AMOUNT);
-                builder.setRemark(REMARK);
-                Result<String> result = transferService.transfer(builder.build());
-                try {
-                    checkResultStatus(result);
-                    successTotal++;
-                } catch (TestFailException e) {
-                    Log.error("创建交易失败:{}",e.getMessage());
-                }
+            for(int s=0;s < 10;s++){
+                ThreadUtils.createAndRunThread("batch-transfer", () -> {
+                    int i = doneTotal.getAndIncrement();
+                    while (i < count) {
+                        int index = i % batchCreateAccountCase.getFormList().size();
+                        String formAddress = batchCreateAccountCase.getFormList().get(index);
+                        String toAddress = batchCreateAccountCase.getToList().get(index);
+                        TransferReq.TransferReqBuilder builder =
+                                new TransferReq.TransferReqBuilder(config.getChainId(), config.getAssetsId())
+                                        .addForm(formAddress, Constants.PASSWORD, TRANSFER_AMOUNT)
+                                        .addTo(toAddress, TRANSFER_AMOUNT);
+                        builder.setRemark(REMARK);
+                        Result<String> result = transferService.transfer(builder.build());
+                        try {
+                            checkResultStatus(result);
+                            successTotal.getAndIncrement();
+                        } catch (TestFailException e) {
+                            Log.error("创建交易失败:{}",e.getMessage());
+                        }
+                        i = doneTotal.getAndIncrement();
+                    }
+                    latch.countDown();
+                });
             }
+//            try {
+//                latch.await();
+//            } catch (InterruptedException e) {
+//                e.printStackTrace();
+//            }
             Log.info("创建{}笔交易,成功{}笔，消耗时间:{}", count,successTotal, System.currentTimeMillis() - start);
         });
         return true;
--- test/src/main/java/io/nuls/test/cases/transcation/batch/BatchTestCase.java
+++ test/src/main/java/io/nuls/test/cases/transcation/batch/BatchTestCase.java
@@ -23,10 +23,10 @@ public class BatchTestCase extends TestCaseChain {
     @Override
     public Class<? extends TestCaseIntf>[] testChain() {
         return new Class[]{
-                BatchReadyNodeAccountCase.class,
-//                BatchCreateAccountCase.class,
-//                SleepAdapter.$15SEC.class,
-//                BatchCreateTransferCase.class,
+//                BatchReadyNodeAccountCase.class,
+                BatchCreateAccountCase.class,
+                SleepAdapter.$15SEC.class,
+                BatchCreateTransferCase.class,
                 SleepAdapter.$15SEC.class
         };
     }
@@ -38,9 +38,9 @@ public class BatchTestCase extends TestCaseChain {
 
     @Override
     public Object initParam() {
-//        BatchParam param = new BatchParam();
-//        param.count = 10;
-//        param.formAddressPriKey = config.getTestSeedAccount();
-        return 1000;
+        BatchParam param = new BatchParam();
+        param.count = 10000;
+        param.formAddressPriKey = config.getTestSeedAccount();
+        return param;
     }
 }
