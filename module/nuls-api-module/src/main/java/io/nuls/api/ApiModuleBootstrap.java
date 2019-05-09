66e75ae6fe5a1ce9b747d1b576c7f5bfbf762a37
zhouwei1985 <251556768@qq.com>
2019-05-09 14:16:40 +0800

    update api-module config

--- module/nuls-api-module/src/main/java/io/nuls/api/ApiModuleBootstrap.java
+++ module/nuls-api-module/src/main/java/io/nuls/api/ApiModuleBootstrap.java
@@ -124,7 +124,7 @@ public class ApiModuleBootstrap extends RpcModule {
             initDB();
             ScheduleManager scheduleManager = SpringLiteContext.getBean(ScheduleManager.class);
             scheduleManager.start();
-            Thread.sleep(3000);
+            Thread.sleep(1000);
             JsonRpcServer server = new JsonRpcServer();
             server.startServer(ApiContext.listenerIp, ApiContext.rpcPort);
             TimeUtils.getInstance().start();
@@ -141,6 +141,8 @@ public class ApiModuleBootstrap extends RpcModule {
      * 初始化数据库连接
      */
     private void initDB() {
+        long time1, time2;
+        time1 = System.currentTimeMillis();
         MongoDBTableServiceImpl tableService = SpringLiteContext.getBean(MongoDBTableServiceImpl.class);
         List<ChainInfo> chainList = tableService.getChainList();
         if (chainList == null) {
@@ -148,10 +150,17 @@ public class ApiModuleBootstrap extends RpcModule {
         } else {
             tableService.initCache();
         }
+        time2 = System.currentTimeMillis();
+        Log.info("------init mongodb tables use time:" + (time2 - time1));
     }
 
     @Override
     public RpcModuleState onDependenciesLoss(Module dependenciesModule) {
         return RpcModuleState.Ready;
+    }
+
+    @Override
+    protected long getTryRuningTimeout() {
+        return 60;
     }
 }
