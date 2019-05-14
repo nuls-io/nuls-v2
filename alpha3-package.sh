844bff720a0395e914b0510bc1314cb39f59ad5a
lijun.zhou <zhouj.172@gmail.com>
2019-05-14 19:30:27 +0800

    alpha3打包脚本

---
+++ alpha3-package.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+cd `dirname $0`
+./package -Nb alpha3
+mv NULS-Wallet-linux64-alpha2 NULS-Wallet-linux64-alpha3
+cp -f ./nuls-main.ncf ./NULS-Wallet-linux64-alpha3/nuls.ncf
+tar -cvf NULS-Wallet-linux64-alpha3-main.tar.gz ./NULS-Wallet-linux64-alpha3
+cp -f ./nuls-chain-10.ncf ./NULS-Wallet-linux64-alpha3/nuls.ncf
+tar -cvf NULS-Wallet-linux64-alpha3-chain-10.tar.gz ./NULS-Wallet-linux64-alpha3
+cp -f ./nuls-chain-11.ncf ./NULS-Wallet-linux64-alpha3/nuls.ncf
+tar -cvf NULS-Wallet-linux64-alpha3-chain-11.tar.gz ./NULS-Wallet-linux64-alpha3
+echo "out of"
+echo "NULS-Wallet-linux64-alpha3-main.tar.gz"
+echo "NULS-Wallet-linux64-alpha3-chain-10.tar.gz"
+echo "NULS-Wallet-linux64-alpha3-chain-11.tar.gz"
