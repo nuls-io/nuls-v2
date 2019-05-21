c920bcde11e0ad6f5d67cbd75c6b9050d1e1af71
zhouwei1985 <251556768@qq.com>
2019-05-21 20:05:41 +0800

    1

---
+++ module/nuls-ledger/src/main/java/io/nuls/ledger/utils/LoggerUtil.java
@@ -0,0 +1,59 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2017-2018 nuls.io
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ */
+package io.nuls.ledger.utils;
+
+import ch.qos.logback.classic.Level;
+import io.nuls.core.log.Log;
+import io.nuls.core.log.logback.LoggerBuilder;
+import io.nuls.core.log.logback.NulsLogger;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author lan
+ * @description
+ * @date 2018/12/17
+ **/
+public class LoggerUtil {
+    /**
+     * 日志
+     */
+    private static Map<String, NulsLogger> loggerMap = new HashMap<>();
+    public static String logLevel = "DEBUG";
+    private static final String LOGGER_KEY1 = "ledger";;
+
+    public static NulsLogger logger(int chainId) {
+        if (null == loggerMap.get(LOGGER_KEY1 + chainId)) {
+            createLogger(chainId);
+        }
+        return loggerMap.get(LOGGER_KEY1 + chainId);
+    }
+
+    public static void createLogger(int chainId) {
+        String folderName = "ledger";
+        loggerMap.put(LOGGER_KEY1 + chainId, LoggerBuilder.getLogger(folderName, chainId));
+    }
+}
