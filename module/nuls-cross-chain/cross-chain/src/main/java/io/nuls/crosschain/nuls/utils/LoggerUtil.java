c714a72aff9e239075b1e9deb926b3496ef5807f
tag0313 <981602228@qq.com>
2019-05-21 11:25:01 +0800

    logger update

--- module/nuls-cross-chain/cross-chain/src/main/java/io/nuls/crosschain/nuls/utils/LoggerUtil.java
+++ module/nuls-cross-chain/cross-chain/src/main/java/io/nuls/crosschain/nuls/utils/LoggerUtil.java
@@ -28,10 +28,10 @@ public class LoggerUtil {
      * Initialize log information for a chain
      * @param chain chain info
      * */
-    public static void initLogger(Chain chain,String level) {
+    public static void initLogger(Chain chain) {
         int chainId = chain.getConfig().getChainId();
         String bootFolder = CrossChainConstant.CHAIN + "_" + chainId + "_"+  FOLDER_PREFIX;
-        NulsLogger messageLogger = LoggerBuilder.getLogger(FOLDER_PREFIX, bootFolder, Level.valueOf(level));
+        NulsLogger messageLogger = LoggerBuilder.getLogger(FOLDER_PREFIX, bootFolder);
         chain.setMessageLog(messageLogger);
     }
 }
