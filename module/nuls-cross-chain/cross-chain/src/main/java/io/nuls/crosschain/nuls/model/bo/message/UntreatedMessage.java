08754dade238f1fc8d2870b7260897c6184f2abf
tag0313 <981602228@qq.com>
2019-05-22 13:52:51 +0800

    cross chain commit

--- module/nuls-cross-chain/cross-chain/src/main/java/io/nuls/crosschain/nuls/model/bo/message/UntreatedMessage.java
+++ module/nuls-cross-chain/cross-chain/src/main/java/io/nuls/crosschain/nuls/model/bo/message/UntreatedMessage.java
@@ -1,6 +1,12 @@
 package io.nuls.crosschain.nuls.model.bo.message;
 import io.nuls.crosschain.base.message.base.BaseMessage;
-
+/**
+ * 未处理消息类
+ * Unprocessed message class
+ *
+ * @author tag
+ * 2019/5/22
+ */
 public class UntreatedMessage {
     private int chainId;
     private String nodeId;
