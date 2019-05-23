a21c0f20fb0d1a017b922b2b12dc3fa61955d3ac
MIMIEYES <pierreluohao@gmail.com>
2019-05-23 13:59:18 +0800

    remove unused codes

--- module/nuls-api-module/src/main/java/io/nuls/api/analysis/WalletRpcHandler.java
+++ module/nuls-api-module/src/main/java/io/nuls/api/analysis/WalletRpcHandler.java
@@ -42,7 +42,7 @@ public class WalletRpcHandler {
             byte[] bytes = RPCUtil.decode(blockHex);
             Block block = new Block();
             block.parse(new NulsByteBuffer(bytes));
-            block.getHeader().setSize(bytes.length);
+            //block.getHeader().setSize(bytes.length);
             BlockInfo blockInfo = AnalysisHandler.toBlockInfo(block, chainID);
 
             return Result.getSuccess(null).setData(blockInfo);
@@ -65,7 +65,7 @@ public class WalletRpcHandler {
             byte[] bytes = RPCUtil.decode(blockHex);
             Block block = new Block();
             block.parse(new NulsByteBuffer(bytes));
-            block.getHeader().setSize(bytes.length);
+            //block.getHeader().setSize(bytes.length);
             BlockInfo blockInfo = AnalysisHandler.toBlockInfo(block, chainID);
             return Result.getSuccess(null).setData(blockInfo);
         } catch (Exception e) {
