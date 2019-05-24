b6c7c729f61502d03d83c6cd4520369b1a698702
MIMIEYES <pierreluohao@gmail.com>
2019-05-24 10:28:03 +0800

    add import class

--- module/nuls-smart-contract/src/test/java/io/nuls/contract/mock/helper/ContractHelperMock.java
+++ module/nuls-smart-contract/src/test/java/io/nuls/contract/mock/helper/ContractHelperMock.java
@@ -1,11 +1,12 @@
 package io.nuls.contract.mock.helper;
 
+import io.nuls.contract.helper.ContractHelper;
 import io.nuls.contract.model.bo.ContractBalance;
 import io.nuls.core.rpc.util.RPCUtil;
 
 import java.util.Arrays;
 
-public class ContractHelperMock extends ContractHelper{
+public class ContractHelperMock extends ContractHelper {
 
     public ContractBalance getBalance(int chainId, byte[] address) {
         byte[] currentNonceBytes = Arrays.copyOfRange(address, address.length - 8, address.length);
