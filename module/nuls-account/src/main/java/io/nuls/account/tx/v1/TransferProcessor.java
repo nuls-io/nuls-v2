8f9f4a15618f773015cff6cddb11bb93a3ee1d19
ad19900913 <ad19900913@outlook.com>
2019-05-27 09:50:20 +0800

    Merge branch 'develop' of https://github.com/nuls-io/nuls-v2 into feature/protocol-update
    # Conflicts:
    #	module/nuls-chain-manager/src/main/java/io/nuls/chain/rpc/cmd/TxModuleCmd.java

--- module/nuls-account/src/main/java/io/nuls/account/tx/v1/TransferProcessor.java
+++ module/nuls-account/src/main/java/io/nuls/account/tx/v1/TransferProcessor.java
@@ -1,16 +1,24 @@
 package io.nuls.account.tx.v1;
 
+import io.nuls.account.model.bo.Chain;
+import io.nuls.account.service.TransactionService;
+import io.nuls.account.util.manager.ChainManager;
 import io.nuls.base.data.BlockHeader;
 import io.nuls.base.data.Transaction;
 import io.nuls.core.constant.TxType;
+import io.nuls.core.core.annotation.Autowired;
 import io.nuls.core.core.annotation.Component;
-import io.nuls.core.log.Log;
 import io.nuls.core.rpc.protocol.TransactionProcessor;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Component("TransferProcessorV1")
 public class TransferProcessor implements TransactionProcessor {
+    @Autowired
+    private ChainManager chainManager;
+    @Autowired
+    private TransactionService transactionService;
     @Override
     public int getType() {
         return TxType.TRANSFER;
@@ -18,20 +26,29 @@ public class TransferProcessor implements TransactionProcessor {
 
     @Override
     public List<Transaction> validate(int chainId, List<Transaction> txs, List<Transaction> allTxs, BlockHeader blockHeader) {
-        Log.info("validate v1");
-        return List.of();
+        Chain chain = chainManager.getChain(chainId);
+        List<Transaction> result = new ArrayList<>();
+        for (Transaction tx : txs) {
+            try {
+                if (!transactionService.transferTxValidate(chain.getChainId(), tx)) {
+                    result.add(tx);
+                }
+            } catch (Exception e) {
+                chain.getLogger().error(e);
+                result.add(tx);
+            }
+        }
+        return result;
     }
 
     @Override
     public boolean commit(int chainId, List<Transaction> txs, BlockHeader blockHeader) {
-        Log.info("commit v1");
-        return false;
+        return true;
     }
 
     @Override
     public boolean rollback(int chainId, List<Transaction> txs, BlockHeader blockHeader) {
-        Log.info("rollback v1");
-        return false;
+        return true;
     }
 
 }
