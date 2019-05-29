beca84bc7e99bac1fe349dceaa8a9010d4f9c904
lijun.zhou <zhouj.172@gmail.com>
2019-05-29 10:18:13 +0800

    临时调整RPC超时时间

--- common/nuls-core-rpc/src/main/java/io/nuls/core/rpc/info/Constants.java
+++ common/nuls-core-rpc/src/main/java/io/nuls/core/rpc/info/Constants.java
@@ -69,7 +69,7 @@ public class Constants {
      * 超时毫秒数(1分钟)
      * Timeout millisecond(1 minute)
      */
-    public static final long TIMEOUT_TIMEMILLIS = 45L * MILLIS_PER_SECOND;
+    public static final long TIMEOUT_TIMEMILLIS = 60 * 5L * MILLIS_PER_SECOND;
 
     /**
      * 取消订阅的常量，已经无用
