55f29fa7b2ae1dbf1e819796a5d3e6a14866d599
zhouwei1985 <251556768@qq.com>
2019-06-03 13:57:27 +0800

    Revert "update  module.ncf"
    This reverts commit adc60b88

--- module/nuls-api-module/src/main/java/io/nuls/api/task/QueryChainInfoTask.java
+++ module/nuls-api-module/src/main/java/io/nuls/api/task/QueryChainInfoTask.java
@@ -2,10 +2,12 @@ package io.nuls.api.task;
 
 
 import io.nuls.api.analysis.WalletRpcHandler;
+import io.nuls.api.cache.ApiCache;
 import io.nuls.api.manager.CacheManager;
 import io.nuls.api.model.po.db.AssetInfo;
 import io.nuls.core.basic.Result;
 
+import java.util.HashMap;
 import java.util.Map;
 
 public class QueryChainInfoTask implements Runnable {
@@ -19,10 +21,17 @@ public class QueryChainInfoTask implements Runnable {
 
     @Override
     public void run() {
-        Result<Map<String, AssetInfo>> result = WalletRpcHandler.getRegisteredChainInfoList();
-        if (result.isSuccess()) {
-            Map<String, AssetInfo> map = result.getData();
-            CacheManager.setAssetInfoMap(map);
+        Map<String, AssetInfo> map;
+        try {
+            Result<Map<String, AssetInfo>> result = WalletRpcHandler.getRegisteredChainInfoList();
+            if (result.isSuccess()) {
+                map = result.getData();
+                CacheManager.setAssetInfoMap(map);
+            }
+        } catch (Exception e) {
+            map = new HashMap<>();
+            ApiCache apiCache = CacheManager.getCache(chainId);
+            map.put(apiCache.getChainInfo().getDefaultAsset().getKey(), apiCache.getChainInfo().getDefaultAsset());
         }
     }
 }
