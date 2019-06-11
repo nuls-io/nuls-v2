8068ef6f0fc6ea89023857c5087e78573f7fb975
tag0313 <981602228@qq.com>
2019-06-11 17:46:39 +0800

    consensus commit

--- module/nuls-consensus/nuls-consensus-poc/src/main/java/io/nuls/poc/utils/thread/process/ConsensusProcess.java
+++ module/nuls-consensus/nuls-consensus-poc/src/main/java/io/nuls/poc/utils/thread/process/ConsensusProcess.java
@@ -166,7 +166,7 @@ public class ConsensusProcess {
      * Otherwise, if the block from the previous node has not been received after waiting for a certain time, it will be packed directly.
      */
     private void waitReceiveNewestBlock(Chain chain, MeetingMember self, MeetingRound round) {
-        long timeout = chain.getConfig().getPackingInterval() / 5;
+        long timeout = chain.getConfig().getPackingInterval() / 2;
         long endTime = self.getPackStartTime() + timeout;
         boolean hasReceiveNewestBlock;
         if (NulsDateUtils.getCurrentTimeSeconds() >= endTime) {
