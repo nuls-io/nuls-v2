20d33bdfb985af01f59716bf8d01fda13a6a9f92
tag0313 <981602228@qq.com>
2019-06-13 17:23:52 +0800

    rpc commit

--- common/nuls-core-rpc/src/main/java/io/nuls/core/rpc/netty/thread/RequestOnlyProcessor.java
+++ common/nuls-core-rpc/src/main/java/io/nuls/core/rpc/netty/thread/RequestOnlyProcessor.java
@@ -3,7 +3,13 @@ import io.nuls.core.log.Log;
 import io.nuls.core.rpc.model.message.Request;
 import io.nuls.core.rpc.netty.channel.ConnectData;
 import io.nuls.core.rpc.netty.processor.RequestMessageProcessor;
-
+/**
+ * 不需要回执的请求处理线程
+ * Request processing threads that do not require a receipt
+ *
+ * @author tag
+ * @date 2019/6/13
+ */
 public class RequestOnlyProcessor implements Runnable{
     private ConnectData connectData;
 
