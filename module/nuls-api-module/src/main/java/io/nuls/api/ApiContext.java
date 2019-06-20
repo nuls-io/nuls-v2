a9d32e1c8d9ad58e6d37f54255fbeebb6658c5bd
zhouwei1985 <251556768@qq.com>
2019-06-20 14:02:12 +0800

    update api

--- module/nuls-api-module/src/main/java/io/nuls/api/ApiContext.java
+++ module/nuls-api-module/src/main/java/io/nuls/api/ApiContext.java
@@ -71,6 +71,8 @@ public class ApiContext {
     public static boolean isRunSmartContract;
 
     public static boolean isRunCrossChain;
+
+    public static boolean isReady;
     //开发者节点地址
     public static Set<String> DEVELOPER_NODE_ADDRESS = new HashSet<>();
     //大使节点地址
