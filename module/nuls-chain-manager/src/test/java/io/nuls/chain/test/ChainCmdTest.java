efb21d8b26e830dfdd4c52eef233f80fbb671d07
lanjinsheng <lanjinsheng@nuls.io>
2019-06-26 14:55:55 +0800

    update

--- module/nuls-chain-manager/src/test/java/io/nuls/chain/test/ChainCmdTest.java
+++ module/nuls-chain-manager/src/test/java/io/nuls/chain/test/ChainCmdTest.java
@@ -73,7 +73,7 @@ public class ChainCmdTest {
         parameters.put("txConfirmedBlockNum",10);
 
         parameters.put("maxSignatureCount",100);
-        parameters.put("signatureByzantineRatio",67);
+        parameters.put("signatureBFTRatio",67);
         parameters.put("verifierList","M9busmCRxEW1oNRdnw5pGCEpMxu9hymtMua,M9busmQ1nWLcriD5VyGWgor2JFMoGsUmgR8");
 
         parameters.put("address","tNULSeBaMoodYW7AqyJrgYdWiJ6nfwfVHHHyXm");
