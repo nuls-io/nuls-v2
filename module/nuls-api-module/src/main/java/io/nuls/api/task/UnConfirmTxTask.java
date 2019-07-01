a79e11f55969b1b4770bc7f6cdde3efacd0e0d52
vivi <251556768@qq.com>
2019-07-01 20:18:11 +0800

    update api

--- module/nuls-api-module/src/main/java/io/nuls/api/task/UnConfirmTxTask.java
+++ module/nuls-api-module/src/main/java/io/nuls/api/task/UnConfirmTxTask.java
@@ -6,6 +6,7 @@ import io.nuls.api.model.po.db.TxHexInfo;
 import io.nuls.api.utils.LoggerUtil;
 import io.nuls.core.basic.Result;
 import io.nuls.core.core.ioc.SpringLiteContext;
+import io.nuls.core.model.DateUtils;
 import io.nuls.core.rpc.util.NulsDateUtils;
 
 import java.util.List;
@@ -24,11 +25,12 @@ public class UnConfirmTxTask implements Runnable {
     @Override
     public void run() {
         try {
+
             List<TxHexInfo> txHexInfoList = transactionService.getUnConfirmList(chainId);
             long currentTime = NulsDateUtils.getCurrentTimeMillis();
             for (int i = txHexInfoList.size() - 1; i >= 0; i--) {
                 TxHexInfo txHexInfo = txHexInfoList.get(i);
-                if (txHexInfo.getTime() < currentTime) {
+                if (txHexInfo.getTime() < currentTime && txHexInfo.getTime() + DateUtils.TEN_MINUTE_TIME > currentTime) {
                     Result result = WalletRpcHandler.validateTx(chainId, txHexInfo.getTxHex());
                     if (!result.isSuccess()) {
                         transactionService.deleteUnConfirmTx(chainId, txHexInfo.getTxHash());
@@ -36,6 +38,8 @@ public class UnConfirmTxTask implements Runnable {
                     } else {
                         WalletRpcHandler.broadcastTx(chainId, txHexInfo.getTxHex());
                     }
+                } else if (txHexInfo.getTime() + DateUtils.TEN_MINUTE_TIME < currentTime) {
+                    transactionService.deleteUnConfirmTx(chainId, txHexInfo.getTxHash());
                 }
             }
         } catch (Exception e) {
