dd3a026c6045b29870b2a8117f3b375d3c1f3e1f
Charlie <29900478@qq.com>
2019-07-04 14:06:02 +0800

    update

--- common/nuls-core-rockdb/src/main/java/io/nuls/core/rockdb/manager/RocksDBManager.java
+++ common/nuls-core-rockdb/src/main/java/io/nuls/core/rockdb/manager/RocksDBManager.java
@@ -609,11 +609,12 @@ public class RocksDBManager {
     private static synchronized Options getCommonOptions(final boolean createIfMissing) {
         Options options = new Options();
 
-        /*options.setCreateIfMissing(createIfMissing);
+        /*
         options.setMaxBackgroundCompactions(4);
         options.setMaxBackgroundFlushes(1);
         options.setMaxOpenFiles(-1);*/
 
+        options.setCreateIfMissing(createIfMissing);
         options.setAllowMmapReads(true);
         options.setCompressionType(CompressionType.NO_COMPRESSION);
         options.setMaxOpenFiles(-1);
