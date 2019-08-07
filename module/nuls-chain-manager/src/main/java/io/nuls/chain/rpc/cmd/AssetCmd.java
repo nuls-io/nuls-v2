039436b3544d4cd9f197fa5d8de2821695ddd1c7
lanjinsheng <lanjinsheng@nuls.io>
2019-08-07 13:53:43 +0800

    update- fix disable chain manage asset bug

--- module/nuls-chain-manager/src/main/java/io/nuls/chain/rpc/cmd/AssetCmd.java
+++ module/nuls-chain-manager/src/main/java/io/nuls/chain/rpc/cmd/AssetCmd.java
@@ -177,6 +177,7 @@ public class AssetCmd extends BaseChainCmd {
             }
             CoinData coinData = this.getDisableCoinData(asset, CmRuntimeInfo.getMainIntChainId(), CmRuntimeInfo.getMainIntAssetId(), tx.size(), accountBalance);
             tx.setCoinData(coinData.serialize());
+            tx.setTime(NulsDateUtils.getCurrentTimeSeconds());
 
             /* 判断签名是否正确 (Determine if the signature is correct) */
             ErrorCode acErrorCode = rpcService.transactionSignature(CmRuntimeInfo.getMainIntChainId(), (String) params.get("address"), (String) params.get("password"), tx);
