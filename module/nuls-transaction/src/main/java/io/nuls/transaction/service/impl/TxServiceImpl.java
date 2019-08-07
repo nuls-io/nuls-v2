796c67a9b6363f4db48c6d9677f928b09dc40213
tag0313 <981602228@qq.com>
2019-08-07 17:39:57 +0800

    cross chain fee update

--- module/nuls-transaction/src/main/java/io/nuls/transaction/service/impl/TxServiceImpl.java
+++ module/nuls-transaction/src/main/java/io/nuls/transaction/service/impl/TxServiceImpl.java
@@ -289,7 +289,7 @@ public class TxServiceImpl implements TxService {
         //验证签名
         validateTxSignature(tx, txRegister, chain);
         //如果有coinData, 则进行验证,有一些交易(黄牌)没有coinData数据
-        if (tx.getType() == TxType.YELLOW_PUNISH || tx.getType() == TxType.VERIFIER_CHANGE) {
+        if (tx.getType() == TxType.YELLOW_PUNISH || tx.getType() == TxType.VERIFIER_CHANGE || tx.getType() == TxType.VERIFIER_INIT) {
             return;
         }
         //coinData基础验证以及手续费 (from中所有的nuls资产-to中所有nuls资产)
