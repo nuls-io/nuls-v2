63cda45e7a89dec15a0b79fdfda56f193855e8ab
vivi <251556768@qq.com>
2019-08-13 15:14:10 +0800

    update readme

--- module/nuls-api-module/src/main/java/io/nuls/api/task/StatisticalNulsTask.java
+++ module/nuls-api-module/src/main/java/io/nuls/api/task/StatisticalNulsTask.java
@@ -7,6 +7,7 @@ import io.nuls.api.db.AccountService;
 import io.nuls.api.db.AgentService;
 import io.nuls.api.manager.CacheManager;
 import io.nuls.api.model.po.db.CoinContextInfo;
+import io.nuls.api.utils.LoggerUtil;
 import io.nuls.base.basic.AddressTool;
 import io.nuls.core.core.ioc.SpringLiteContext;
 
@@ -29,24 +30,29 @@ public class StatisticalNulsTask implements Runnable {
 
     @Override
     public void run() {
-        BigInteger totalCoin = accountService.getAllAccountBalance(chainId);
-        BigInteger consensusTotal = agentService.getConsensusCoinTotal(chainId);
+        try {
+            BigInteger totalCoin = accountService.getAllAccountBalance(chainId);
+            BigInteger consensusTotal = agentService.getConsensusCoinTotal(chainId);
 
-        ApiCache apiCache = CacheManager.getCache(chainId);
-        CoinContextInfo contextInfo = apiCache.getCoinContextInfo();
-        //团队持有数量
-        BigInteger teamNuls = accountService.getAccountTotalBalance(chainId, ApiContext.TEAM_ADDRESS);
-        //销毁数量
-        byte[] address = AddressTool.getAddress(ApiContext.blackHolePublicKey, chainId);
-        String destroyAddress = AddressTool.getStringAddressByBytes(address);
-        BigInteger destroyNuls = accountService.getAccountTotalBalance(chainId, destroyAddress);
-        //商务持有数量
-        BigInteger businessNuls = accountService.getAccountTotalBalance(chainId, ApiContext.BUSINESS_ADDRESS);
-        //社区持有数量
-        BigInteger communityNuls = accountService.getAccountTotalBalance(chainId, ApiContext.COMMUNITY_ADDRESS);
+            ApiCache apiCache = CacheManager.getCache(chainId);
+            CoinContextInfo contextInfo = apiCache.getCoinContextInfo();
+            //团队持有数量
+            BigInteger teamNuls = accountService.getAccountTotalBalance(chainId, ApiContext.TEAM_ADDRESS);
+            //销毁数量
+            byte[] address = AddressTool.getAddress(ApiContext.blackHolePublicKey, chainId);
+            String destroyAddress = AddressTool.getStringAddressByBytes(address);
+            BigInteger destroyNuls = accountService.getAccountTotalBalance(chainId, destroyAddress);
+            //商务持有数量
+            BigInteger businessNuls = accountService.getAccountTotalBalance(chainId, ApiContext.BUSINESS_ADDRESS);
+            //社区持有数量
+            BigInteger communityNuls = accountService.getAccountTotalBalance(chainId, ApiContext.COMMUNITY_ADDRESS);
 
-        contextInfo.setTotal(totalCoin);
-        contextInfo.setConsensusTotal(consensusTotal);
-        contextInfo.setCirculation(totalCoin.subtract(teamNuls).subtract(destroyNuls).subtract(businessNuls).subtract(communityNuls));
+            contextInfo.setTotal(totalCoin);
+            contextInfo.setConsensusTotal(consensusTotal);
+            contextInfo.setCirculation(totalCoin.subtract(teamNuls).subtract(destroyNuls).subtract(businessNuls).subtract(communityNuls));
+        }catch (Exception e) {
+            LoggerUtil.commonLog.error(e);
+        }
+
     }
 }
