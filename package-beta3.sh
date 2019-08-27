be8ac2d31fc3920c26ee74744ce70e50784a7670
lijun.zhou <zhouj.172@gmail.com>
2019-08-27 17:52:32 +0800

    beta3.2 平行链配置

--- package-beta3.sh
+++ package-beta3.sh
@@ -28,22 +28,22 @@ then
 else
     tar -zcPf ${OUT_DIR}-main-${TARGET_OS}.tar.gz ./${OUT_DIR}
 fi
-#rm -rf ${OUT_DIR}
-#./package -r chain-manager
-#if [ -n "$2" ];
-#then
-#./package -O $TARGET_OS -Nb $version -J $2 -o ./$OUT_DIR
-#else
-#./package -O $TARGET_OS -Nb $version -o ./$OUT_DIR
-#fi
-#cp -f ./nuls-chain-10-$version.ncf ./${OUT_DIR}/nuls.ncf
-#cp -f ./genesis-block-10-$version.json ./${OUT_DIR}/genesis-block.json
-#if [ "$TARGET_OS" == "window" ];
-#then
-#    zip -r ${OUT_DIR}-NBTC-${TARGET_OS}.zip ./${OUT_DIR}
-#else
-#    tar -zcPf ${OUT_DIR}-NBTC-${TARGET_OS}.tar.gz ./${OUT_DIR}
-#fi
+rm -rf ${OUT_DIR}
+./package -r chain-manager
+if [ -n "$2" ];
+then
+./package -O $TARGET_OS -Nb $version -J $2 -o ./$OUT_DIR
+else
+./package -O $TARGET_OS -Nb $version -o ./$OUT_DIR
+fi
+cp -f ./nuls-chain-10-$version.ncf ./${OUT_DIR}/nuls.ncf
+cp -f ./genesis-block-10-$version.json ./${OUT_DIR}/genesis-block.json
+if [ "$TARGET_OS" == "window" ];
+then
+    zip -r ${OUT_DIR}-NBTC-${TARGET_OS}.zip ./${OUT_DIR}
+else
+    tar -zcPf ${OUT_DIR}-NBTC-${TARGET_OS}.tar.gz ./${OUT_DIR}
+fi
 echo "======================== out of ======================== "
 if [ "$TARGET_OS" == "window" ];
 then
