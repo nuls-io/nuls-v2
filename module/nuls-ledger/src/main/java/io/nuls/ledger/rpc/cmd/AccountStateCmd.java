1f11b30a18f58e8da5b0250e4c737cbd9a9a719e
lanjinsheng <lanjinsheng@nuls.io>
2019-09-20 10:02:57 +0800

    update- getNonce parameter

--- module/nuls-ledger/src/main/java/io/nuls/ledger/rpc/cmd/AccountStateCmd.java
+++ module/nuls-ledger/src/main/java/io/nuls/ledger/rpc/cmd/AccountStateCmd.java
@@ -188,7 +188,9 @@ public class AccountStateCmd extends BaseLedgerCmd {
         rtMap.put("list", resultList);
         return success(rtMap);
     }
-
+    public static void main(String []args){
+        System.out.println(Boolean.valueOf("false"));
+    }
     /**
      * 获取账户nonce值
      * get user account nonce
@@ -220,7 +222,7 @@ public class AccountStateCmd extends BaseLedgerCmd {
         Integer assetId = (Integer) params.get("assetId");
         boolean isConfirmed = false;
         if (null != params.get("isConfirmed")) {
-            isConfirmed = Boolean.getBoolean(params.get("isConfirmed").toString());
+            isConfirmed = Boolean.valueOf(params.get("isConfirmed").toString());
         }
         if (!chainHanlder(chainId)) {
             return failed(LedgerErrorCode.CHAIN_INIT_FAIL);
@@ -263,7 +265,7 @@ public class AccountStateCmd extends BaseLedgerCmd {
         Integer assetId = (Integer) params.get("assetId");
         boolean isConfirmed = false;
         if (null != params.get("isConfirmed")) {
-            isConfirmed = Boolean.getBoolean(params.get("isConfirmed").toString());
+            isConfirmed = Boolean.valueOf(params.get("isConfirmed").toString());
         }
         if (!chainHanlder(chainId)) {
             return failed(LedgerErrorCode.CHAIN_INIT_FAIL);
