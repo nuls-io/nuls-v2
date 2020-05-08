866c9cffeab8b123cd2d0deb9613e3dda52e2675
tag0313 <981602228@qq.com>
2020-05-08 18:25:48 +0800

    chainId update

--- module/nuls-consensus/nuls-consensus-poc/src/main/java/io/nuls/poc/service/impl/DepositServiceImpl.java
+++ module/nuls-consensus/nuls-consensus-poc/src/main/java/io/nuls/poc/service/impl/DepositServiceImpl.java
@@ -83,7 +83,7 @@ public class DepositServiceImpl implements DepositService {
             return Result.getFailed(ConsensusErrorCode.CHAIN_NOT_EXIST);
         }
         try {
-            if (!AddressTool.validAddress((short) dto.getChainId(), dto.getAddress())) {
+            if (!AddressTool.validAddress(dto.getChainId(), dto.getAddress())) {
                 throw new NulsException(ConsensusErrorCode.ADDRESS_ERROR);
             }
             //账户验证
@@ -171,7 +171,7 @@ public class DepositServiceImpl implements DepositService {
             return Result.getFailed(ConsensusErrorCode.CHAIN_NOT_EXIST);
         }
         try {
-            if (!AddressTool.validAddress((short) dto.getChainId(), dto.getAddress())) {
+            if (!AddressTool.validAddress( dto.getChainId(), dto.getAddress())) {
                 return Result.getFailed(ConsensusErrorCode.PARAM_ERROR);
             }
             //账户验证
