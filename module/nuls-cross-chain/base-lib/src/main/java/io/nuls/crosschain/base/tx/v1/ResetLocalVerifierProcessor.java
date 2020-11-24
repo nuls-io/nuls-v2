447cf231864e2d17acefb762de98e3a744494845
lijun.zhou <zhouj.172@gmail.com>
2020-11-24 11:55:40 +0800

    重置本链验证人列表交易

--- module/nuls-cross-chain/base-lib/src/main/java/io/nuls/crosschain/base/tx/v1/ResetLocalVerifierProcessor.java
+++ module/nuls-cross-chain/base-lib/src/main/java/io/nuls/crosschain/base/tx/v1/ResetLocalVerifierProcessor.java
@@ -1,5 +1,6 @@
 package io.nuls.crosschain.base.tx.v1;
 
+import com.google.common.collect.Maps;
 import io.nuls.base.data.BlockHeader;
 import io.nuls.base.data.Transaction;
 import io.nuls.base.protocol.TransactionProcessor;
@@ -8,6 +9,7 @@ import io.nuls.core.core.annotation.Autowired;
 import io.nuls.core.core.annotation.Component;
 import io.nuls.crosschain.base.service.ResetLocalVerifierService;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -29,17 +31,23 @@ public class ResetLocalVerifierProcessor implements TransactionProcessor {
 
     @Override
     public Map<String, Object> validate(int chainId, List<Transaction> txs, Map<Integer, List<Transaction>> txMap, BlockHeader blockHeader) {
-        return resetLocalVerifierService.validate(chainId,txs,blockHeader);
+        if (txs.isEmpty()) {
+            Map<String,Object> result = Maps.newHashMap();
+            result.put("txList", txs);
+            result.put("errorCode", null);
+            return result;
+        }
+        return resetLocalVerifierService.validate(chainId, txs, blockHeader);
     }
 
     @Override
     public boolean commit(int chainId, List<Transaction> txs, BlockHeader blockHeader) {
-        return resetLocalVerifierService.commitTx(chainId,txs,blockHeader);
+        return resetLocalVerifierService.commitTx(chainId, txs, blockHeader);
     }
 
     @Override
     public boolean rollback(int chainId, List<Transaction> txs, BlockHeader blockHeader) {
-        return resetLocalVerifierService.rollbackTx(chainId,txs,blockHeader);
+        return resetLocalVerifierService.rollbackTx(chainId, txs, blockHeader);
     }
 
 }
