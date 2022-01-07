9e611e2e1e3fa023ae808259f91cf07b8bfd3adf
niels1286 <niels@nuls.io>
2022-01-07 14:00:17 +0800

    remove vm log

--- module/nuls-smart-contract/src/main/java/io/nuls/contract/vm/VM.java
+++ module/nuls-smart-contract/src/main/java/io/nuls/contract/vm/VM.java
@@ -25,7 +25,6 @@
 package io.nuls.contract.vm;
 
 import io.nuls.contract.model.dto.BlockHeaderDto;
-import io.nuls.contract.util.Log;
 import io.nuls.contract.util.VMContext;
 import io.nuls.contract.vm.code.MethodCode;
 import io.nuls.contract.vm.code.VariableType;
@@ -226,22 +225,12 @@ public class VM {
 
     public void run(ObjectRef objectRef, MethodCode methodCode, VMContext vmContext, ProgramInvoke programInvoke) {
         this.vmContext = vmContext;
-        //todo Niels
-        long startTime = System.nanoTime();
         Object[] runArgs = runArgs(objectRef, methodCode, programInvoke.getArgs());
-        long use = System.nanoTime()-startTime;
-        Log.info("===================================================================step 1.1 : {}ns",use);
-        startTime = System.nanoTime();
         if (isEnd()) {
             return;
         }
         initProgramContext(programInvoke);
-        use = System.nanoTime()-startTime;
-        Log.info("===================================================================step 1.2 : {}ns",use);
-        startTime = System.nanoTime();
         run(methodCode, runArgs, true);
-        use = System.nanoTime()-startTime;
-        Log.info("===================================================================step 1.3 : {}ns",use);
     }
 
     private Object[] runArgs(ObjectRef objectRef, MethodCode methodCode, String[][] args) {
