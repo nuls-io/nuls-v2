c4d33fb6b6f78221e8615e596bc6a1b223ca4c4d
cobble586 <cobble2626@gmail.com>
2024-02-18 22:08:46 +0800

    add cross asset cmd check

--- module/nuls-cores/src/main/java/io/nuls/chain/rpc/cmd/AssetCmd.java
+++ module/nuls-cores/src/main/java/io/nuls/chain/rpc/cmd/AssetCmd.java
@@ -201,9 +201,9 @@ public class AssetCmd extends BaseChainCmd {
             if (dbChain.isDelete()) {
                 return failed(CmErrorCode.ERROR_CHAIN_REG_CMD);
             }
-            if (assetService.assetExist(asset) && asset.isAvailable()) {
-                return failed(CmErrorCode.ERROR_ASSET_ID_EXIST);
-            }
+            //if (assetService.assetExist(asset) && asset.isAvailable()) {
+            //    return failed(CmErrorCode.ERROR_ASSET_ID_EXIST);
+            //}
             /* 组装交易发送 (Send transaction) */
             Transaction tx = new AddAssetToChainTransaction();
             LoggerUtil.COMMON_LOG.debug("version= {}", ChainManagerUtil.getVersion(CmRuntimeInfo.getMainIntChainId()));
